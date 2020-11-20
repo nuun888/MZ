4afab291676bf8056d0edcb2adeb8645effd6c15
nuun888 <70774024+nuun888@users.noreply.github.com>
2020-11-20 22:11:18 +0900

    Add files via upload

--- NUUN_CommandIcon.js
+++ NUUN_CommandIcon.js
@@ -5,14 +5,51 @@
  * This software is released under the MIT License.
  * http://opensource.org/licenses/mit-license.php
  * -------------------------------------------------------------------------------------
- * 
- * 更新履歴
- * 2020/11/19 Ver 1.0.0
- *  初版
- * 
- * 2020/11/20 Ver 1.0.1
- *  プラグインパラメータのCommadIconが空白だった場合、エラーが出る問題を修正。
  */ 
+/*:
+ * @target MZ
+ * @plugindesc CommadIcon
+ * @author NUUN
+ * 
+ * @help
+ * Display the icon in the command menu.
+ * If you are using a version other than the Japanese version,
+ * Enter the name displayed in the command menu in "Command Name".
+ * 
+ * Terms of service
+ * This plugin is distributed under the MIT license.
+ * 
+ * Change log
+ * 11/19/2020 Ver 1.0.0
+ * 
+ * 11/20/2020 Ver 1.0.1
+ *  Fixed an issue that caused an error if the plugin parameter CommadIcon was blank.
+ * 
+ * 11/20/2020 Ver 1.0.2
+ *  Fixed because it was not multilingual.
+ * 
+ * @param CommadIcon
+ * @text Command icon settings
+ * @desc Set the icon to be displayed in the command.
+ * @default []
+ * @type struct<CommadIconList>[]
+ * 
+ */
+/*~struct~CommadIconList:
+ * 
+ * @param CommadName
+ * @text Command name
+ * @desc Command name to display the icon (Please use the same name as the command name to be displayed)
+ * @type string
+ * 
+ * @param iconId
+ * @text Icon index number
+ * @desc The index number of the icon.
+ * @type number
+ * @default 0
+ * 
+ */
+
 /*:ja
  * @target MZ
  * @plugindesc コマンドアイコン
@@ -24,6 +61,15 @@
  * 利用規約
  * このプラグインはMITライセンスで配布しています。
  * 
+ * 更新履歴
+ * 2020/11/19 Ver 1.0.0
+ *  初版
+ * 
+ * 2020/11/20 Ver 1.0.1
+ *  プラグインパラメータのCommadIconが空白だった場合、エラーが出る問題を修正。
+ * 
+ * 2020/11/20 Ver 1.0.2
+ *  多言語対応ではなかったため修正。
  * 
  * @param CommadIcon
  * @text コマンドアイコン設定
@@ -32,7 +78,7 @@
  * @type struct<CommadIconList>[]
  * 
  */
-/*~struct~CommadIconList:
+/*~struct~CommadIconList:ja
  * 
  * @param CommadName
  * @text コマンド名

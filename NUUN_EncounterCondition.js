b1ee2b5925e9af68701d8de8b172b9a61a525529
nuun888 <70774024+nuun888@users.noreply.github.com>
2021-01-04 01:07:58 +0900

    Add files via upload

--- NUUN_EncounterCondition.js
+++ NUUN_EncounterCondition.js
@@ -1,16 +1,16 @@
 /*:-----------------------------------------------------------------------------------
  * NUUN_EncounterCondition.js
  * 
- * Copyright (C) 2020 NUUN
+ * Copyright (C) 2021 NUUN
  * This software is released under the MIT License.
  * http://opensource.org/licenses/mit-license.php
  * -------------------------------------------------------------------------------------
  * 
  * 更新履歴
- * 2020/11 Ver 1.0.0
+ * 2021/1/4 Ver.1.0.0
+ * 初版
  */ 
 /*:
- * 
  * @target MZ
  * @plugindesc エンカウント条件
  * @author NUUN
@@ -18,16 +18,21 @@
  * @help
  * 敵グループに対してエンカウント条件を付けます。
  * 敵グループの１ページ目に注釈で以下のタグを記入してください。
- * 条件が複数の場合、全ての条件に合致した場合のみエンカウントします。
+ * 条件が複数の場合、全ての条件に合致した場合のみエンカウント候補になります。
  * 
+ * <Enc_Switch:[id], [flags]>　[id]:スイッチID、[flags]true及びfalse
  * <Enc_Switch:3, true> スイッチのID３がtrueの時、エンカウントします。
+ * 
+ * <Enc_Val:[id], [value], [Inequality]>　[id]:　変数ID, [value]:数値, [Inequality]:不等号
  * <Enc_Val:3, 6, equal> 変数のID３の値が6と同じ場合、エンカウントします。
  * <Enc_Val:3, 6, greater> 変数のID３の値が6より大きい場合、エンカウントします。
  * <Enc_Val:3, 6, less> 変数のID３の値が6未満場合、エンカウントします。
  * <Enc_Val:3, 6, greaterEqual> 変数のID３の値が6以上場合、エンカウントします。
  * <Enc_Val:3, 6, lessEqual> 変数のID３の値が6以下場合、エンカウントします。
- * 演算識別がない場合は変数＞値で評価します。
- * <Enc_eval:条件式> 条件式に合致する場合、エンカウントします。
+ * 不等号識別がない場合は変数＞値で評価します。
+ * 
+ * <Enc_eval:[eval]> 条件式に合致する場合、エンカウントします。[eval]:条件式
+ * <Enc_eval:$gamePlayer.isInShip()>プレイヤーが大型船に乗っている時のみエンカウントします。
  * 
  * 利用規約
  * このプラグインはMITライセンスで配布しています。
@@ -39,9 +44,6 @@ Imported.NUUN_EncounterCondition = true;
 
 (() => {
   const parameters = PluginManager.parameters('NUUN_EncounterCondition');
-  const re1 = /<(?:Enc_Switch):\s*(.*)>/;
-  const re2 = /<(?:Enc_Val):\s*(.*)>/;
-  const re3 = /<(?:Enc_eval):\s*(.*)>/;
 
   const _Game_Player_meetsEncounterConditions = Game_Player.prototype.meetsEncounterConditions;
   Game_Player.prototype.meetsEncounterConditions = function(encounter) {
@@ -68,28 +70,29 @@ Imported.NUUN_EncounterCondition = true;
   Game_Player.prototype.conditionsEval = function(conditions) {
     if(conditions.type === 0) {
       const switchId = Number(conditions.deta[0]);
-      const a = conditions.deta[1] ? Boolean(conditions.deta[1]) : true;
-      if(switchId > 0 && $gameSwitches.value(switchId) === a) {
+      const flag = conditions.deta[1] ? Boolean(conditions.deta[1]) : true;
+      if(switchId > 0 && $gameSwitches.value(switchId) === flag) {
         return true;
       }
     } else if(conditions.type === 1) {
       const valId = Number(conditions.deta[0]);
       const val = Number(conditions.deta[1]);
-      const operator = conditions.deta[2] ? conditions.deta[2] : 'greater';
+      const operator = conditions.deta[2] ? String(conditions.deta[2]).trim() : "greater";
       if(valId > 0) {
-        if(operator === 'greater') {
-          return $gameVariables.value(valId) > val;
-        } else if (operator === 'less') {
-          return $gameVariables.value(valId) < val;
-        } else if (operator === 'equal') {
-          return $gameVariables.value(valId) === val;
-        } else if (operator === 'greaterEqual') {
-          return $gameVariables.value(valId) >= val;
-        } else if (operator === 'lessEqual') {
+        switch (operator) {
+          case "greater":
+            return $gameVariables.value(valId) > val;
+          case "less":
+            return $gameVariables.value(valId) < val;
+          case "equal":
+            return $gameVariables.value(valId) === val;
+          case "greaterEqual":
+            return $gameVariables.value(valId) >= val;
+          case "lessEqual": 
           return $gameVariables.value(valId) <= val;
         }
       }
-    } else if(conditions.type === 2) {
+    } else if(conditions.type === 2) {console.log(eval(conditions.deta))
       return eval(conditions.deta);
     }
     return false;
@@ -98,6 +101,9 @@ Imported.NUUN_EncounterCondition = true;
   Game_Player.prototype.encounterConditions = function(troop) {
     const pages = troop.pages[0];
     list = [];
+    const re1 = /<(?:Enc_Switch):\s*(.*)>/;
+    const re2 = /<(?:Enc_Val):\s*(.*)>/;
+    const re3 = /<(?:Enc_eval):\s*(.*)>/;
     pages.list.forEach(function(tag) {
       if (tag.code === 108 || tag.code === 408) {
         let match1 = re1.exec(tag.parameters[0]);
@@ -113,5 +119,5 @@ Imported.NUUN_EncounterCondition = true;
       }
     });
     return list;
-  }
+  };
 })();

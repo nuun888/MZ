5ed6bb5e70832b576288b2e26c7681cea6777ebd
nuun888 <70774024+nuun888@users.noreply.github.com>
2021-02-03 00:11:58 +0900

    Add files via upload

--- NUUN_BattleItemSkillWindowMV.js
+++ NUUN_BattleItemSkillWindowMV.js
@@ -10,13 +10,20 @@
  * @target MZ
  * @plugindesc  戦闘中アイテム、スキル選択画面MV風表示
  * @author NUUN
- * @version 1.0.0
+ * @version 1.0.2
  * 
  * @help
  * 戦闘中のアイテム、スキル選択画面をMV風形式に変更させます。
  * 
+ * 利用規約
+ * このプラグインはMITライセンスで配布しています。
+ * 
  * 
  * 更新履歴
+ * 2021/2/3 Ver.1.0.2
+ * マップ上でアイテム、スキル画面を表示した時、戦闘時の画面が表示されてしまう問題を修正。
+ * 2021/2/1 Ver.1.0.1
+ * キャンセルボタンの処理を修正。
  * 2021/1/31 Ver.1.0.0
  * 初版
  * 
@@ -44,7 +51,6 @@ const parameters = PluginManager.parameters('NUUN_BattleItemSkillWindowMV');
 const VariableHeight = eval(parameters['VariableHeight'] || "false");
 const ItemMaxRow = Number(parameters['ItemMaxRow'] || 6);
 let maxHeight = 0;
-let cancelButton_y = 0;
 
 const _Scene_Battle_initialize = Scene_Battle.prototype.initialize;
 Scene_Battle.prototype.initialize = function() {
@@ -53,7 +59,7 @@ Scene_Battle.prototype.initialize = function() {
 
 const _Scene_Battle_skillWindowRect = Scene_Battle.prototype.skillWindowRect;
 Scene_Battle.prototype.skillWindowRect = function() {
-  const rect = _Scene_Battle_skillWindowRect.call(this);//ItemMaxRow
+  const rect = _Scene_Battle_skillWindowRect.call(this);
   rect.y = this._helpWindow.height;
   maxHeight = Math.min(this.calcWindowHeight(ItemMaxRow, true), Graphics.boxHeight - rect.y - this.windowAreaHeight());
   rect.height = maxHeight;
@@ -63,7 +69,7 @@ Scene_Battle.prototype.skillWindowRect = function() {
 const _Window_ItemList_refresh = Window_ItemList.prototype.refresh;
 Window_ItemList.prototype.refresh = function() {
   _Window_ItemList_refresh.call(this);
-  if (VariableHeight) {
+  if (VariableHeight && $gameParty.inBattle()) {
     this.height = Math.min(maxHeight, this.itemHeight() * Math.ceil(this.maxItems() / this.maxCols()) + $gameSystem.windowPadding() * 2);
   }
 };
@@ -71,7 +77,7 @@ Window_ItemList.prototype.refresh = function() {
 const _Window_SkillList_refresh = Window_SkillList.prototype.refresh;
 Window_SkillList.prototype.refresh = function() {
   _Window_SkillList_refresh.call(this);
-  if (VariableHeight) {
+  if (VariableHeight && $gameParty.inBattle()) {
     this.height = Math.min(maxHeight, this.itemHeight() * Math.ceil(this.maxItems() / this.maxCols()) + $gameSystem.windowPadding() * 2);
   }
 };
@@ -85,15 +91,20 @@ Scene_Battle.prototype.createCancelButton = function() {
 const _Scene_Battle_updateCancelButton = Scene_Battle.prototype.updateCancelButton;
 Scene_Battle.prototype.updateCancelButton = function() {
   _Scene_Battle_updateCancelButton.call(this);
-  if (this._cancelButton) {
-    if (this._skillWindow.visible) {
-      this._cancelButton.y = this._skillWindow.height + cancelButton_y;
-    } else if (this._itemWindow.visible) {
-      this._cancelButton.y = this._itemWindow.height + cancelButton_y;
-    } else{
-      this._cancelButton.y = cancelButton_y;
-    }
+  if (this._cancelButton) {  
+    this._cancelButton.y = this.buttonY();
   }
+};
+
+const _Scene_Battle_buttonY = Scene_Battle.prototype.buttonY;
+Scene_Battle.prototype.buttonY = function() {
+  const y = _Scene_Battle_buttonY.call(this);
+  if (this._skillWindow.visible) {
+    return y + this._skillWindow.height;
+  } else if (this._itemWindow.visible) {
+    return y + this._itemWindow.height;
+  }
+  return y;
 };
 
 const _Scene_Battle_commandSkill = Scene_Battle.prototype.commandSkill;

bf9becd426908527a4b27557842bfb14170bbf03
nuun888 <70774024+nuun888@users.noreply.github.com>
2021-02-03 00:18:29 +0900

    Add files via upload

--- NUUN_CommandIcon.js
+++ NUUN_CommandIcon.js
@@ -8,89 +8,7 @@
  */ 
 /*:
  * @target MZ
- * @plugindesc Command customization plugin
- * @author NUUN
- * 
- * @help
- * You can display an icon in the command menu and change the text color of the command name.
- * You can select the position of the command name from left-aligned, center-aligned, and right-aligned.
- * 
- * 
- * Terms of service
- * This plugin is distributed under the MIT license.
- * 
- * Change log
- * 11/19/2020 Ver 1.0.0
- * 
- * 11/20/2020 Ver 1.0.1
- *  Fixed an issue that caused an error if the plugin parameter CommadIcon was blank.
- * 
- * 11/20/2020 Ver 1.0.2
- *  Fixed because it was not multilingual.
- * 
- * 11/21/2020 Ver 1.1.0
- *  Added the function to color the command name.
- * 
- * 11/22/2020 Ver 1.1.1
- *  Added a function to select from left alignment, center alignment, and right alignment.
- * 
- * @param CommadIcon
- * @text Command icon settings
- * @desc Set the icon to be displayed in the command.
- * @default []
- * @type struct<CommadIconList>[]
- * 
- * @param CommandPosition
- * @text Command name display position of vertical command.
- * @desc Specifies the display position of the command name of the vertical command. (Menu screen, etc.)
- * @type select
- * @option Left
- * @value 0
- * @option Center
- * @value 1
- * @option Right
- * @value 2
- * @default 1
- * 
- * @param HorzCommandPosition
- * @text Command name display position of horizontal command.
- * @desc Specifies the display position of the command name of the horizontal command. (Item column etc.)
- * @type select
- * @option Left
- * @value 0
- * @option Center
- * @value 1
- * @option Right
- * @value 2
- * @default 1
- * 
- */
-/*~struct~CommadIconList:
- * 
- * @param CommadName
- * @text Command name
- * @desc Command name to display the icon (Please use the same name as the command name to be displayed)
- * @type string
- * 
- * @param CommadNameColor
- * @text Command name color
- * @desc Command name color index number.
- * @type number
- * @default 0
- * @min 0
- * 
- * @param iconId
- * @text Icon index number
- * @desc The index number of the icon.
- * @type number
- * @default 0
- * @min 0
- * 
- */
-
-/*:ja
- * @target MZ
- * @plugindesc コマンドカスタマイズプラグイン
+ * @plugindesc コマンド拡張
  * @author NUUN
  * 
  * @help
@@ -101,20 +19,16 @@
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
- * 2020/11/19 Ver 1.0.0
- *  初版
- * 
- * 2020/11/20 Ver 1.0.1
- *  プラグインパラメータのCommadIconが空白だった場合、エラーが出る問題を修正。
- * 
- * 2020/11/20 Ver 1.0.2
- *  多言語対応ではなかったため修正。
- * 
- * 2020/11/21 Ver 1.1.0
- *  コマンド名に色を付ける機能を追加。
- * 
  * 2020/11/22 Ver 1.1.1
- *  コマンド名を左揃え、中央揃え、右揃えから選べる機能を追加。
+ * コマンド名を左揃え、中央揃え、右揃えから選べる機能を追加。
+ * 2020/11/21 Ver 1.1.0
+ * コマンド名に色を付ける機能を追加。
+ * 2020/11/20 Ver 1.0.2
+ * 日本語版しかヘルプやプラグインパラメータが表示されなかった問題を修正。
+ * 2020/11/20 Ver 1.0.1
+ * プラグインパラメータのCommadIconが空白だった場合、エラーが出る問題を修正。
+ * 2020/11/19 Ver 1.0.0
+ * 初版
  * 
  * @param CommadIcon
  * @text コマンドアイコン設定

4e137609f6dfb40ab08db76b5c7619f56e548e77
nuun888 <70774024+nuun888@users.noreply.github.com>
2021-02-07 00:07:45 +0900

    Add files via upload

--- NUUN_ItemNameColor.js
+++ NUUN_ItemNameColor.js
@@ -8,9 +8,9 @@
  */ 
 /*:
  * @target MZ
- * @plugindesc  アイテム、スキル欄文字色個別指定
+ * @plugindesc  アイテム、スキル欄文字色個別変更
  * @author NUUN
- * @version 1.0.0
+ * @version 1.0.1
  * 
  * @help
  * アイテム、スキル欄の文字に色を指定できます。
@@ -21,7 +21,10 @@
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2021/2/7 Ver.1.0.1
+ * テキストカラーを戻す処理のタイミングを変更。
  * 2021/2/6 Ver.1.0.0
+ * 初版
  */
 var Imported = Imported || {};
 Imported.NUUN_ItemNameColor = true;
@@ -50,17 +53,18 @@ Imported.NUUN_ItemNameColor = true;
       this.changeTextColor(this.nameColor);
     }
     _Window_Base_drawText.call(this, text, x, y, maxWidth, align);
-  };
-
-  const _Window_ItemList_drawItemNumber = Window_ItemList.prototype.drawItemNumber;
-  Window_ItemList.prototype.drawItemNumber = function(item, x, y, width) {
-    this.nameColor = 0;
-    this.resetTextColor();
-    _Window_ItemList_drawItemNumber.call(this, item, x, y, width);
+    if (this.nameColor) {
+      this.resetNameColor();
+    }
   };
 
   Window_Base.prototype.changeNameColor = function(item) {
     const color = item.meta.NameColor ? item.meta.NameColor : 0;
     return ColorManager.textColor(color);
+  };
+
+  Window_Base.prototype.resetNameColor = function() {
+    this.nameColor = 0;
+    this.resetTextColor();
   };
 })();

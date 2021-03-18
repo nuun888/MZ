11641c27321574cdfbcab5085f5f805537d73a89
nuun888 <70774024+nuun888@users.noreply.github.com>
2021-03-18 22:05:54 +0900

    Add files via upload

--- NUUN_BigEnemy.js
+++ NUUN_BigEnemy.js
@@ -10,13 +10,17 @@
  * @target MZ
  * @plugindesc 巨大エネミー
  * @author NUUN
- * @version 1.0.3
+ * @version 1.1.0
  *            
  * @help 画面いっぱいにエネミー画像を表示します。
  * エネミーのメモ欄に以下のどちらかを記入してください。
  * <BigEnemy> 画像を画面サイズに合わせます。
  * <BigEnemy:OriginalRatio> 比率を変更せず横幅の倍率基準で拡大します。
- * <BigEnemyY:50> Y座標を調整します。
+ * <BigEnemy:UnderPosition> 画像を原寸倍率で画面下に合わせ表示させます。画像のサイズ調整はいたしません。
+ * 
+ * オプションタグ　エネミーのメモ欄
+ * <BigEnemyFront> 画像を一番手前側に表示させます。
+ * <BigEnemyY:50> Y座標を50下にシフトして表示します。
  * 
  * このプラグインはフロントビューで使用することを前提しています。
  * 
@@ -25,6 +29,9 @@
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2021/3/18 Ver.1.1.0
+ * モンスター画像の下に合わせて表示する機能を追加。
+ * ２回目の戦闘以降にモンスターが手前に表示される問題を修正。
  * 2021/3/17 Ver.1.0.3
  * Y座標を調整できる機能を追加。
  * 2021/1/17 Ver.1.0.2
@@ -43,7 +50,11 @@ Imported.NUUN_BigEnemy = true;
 
   const _Game_Enemy_screenY = Game_Enemy.prototype.screenY;
   Game_Enemy.prototype.screenY = function() {
-    return this.enemy().meta.BigEnemy ? 0 : _Game_Enemy_screenY.call(this);
+    if (this.enemy().meta.BigEnemy) {
+      return this.enemy().meta.BigEnemyFront ? Graphics.height : 0;
+    } else {
+      return _Game_Enemy_screenY.call(this);
+    }
   };
 
   const _Sprite_Battler_initMembers = Sprite_Battler.prototype.initMembers;
@@ -55,43 +66,46 @@ Imported.NUUN_BigEnemy = true;
   const _Sprite_Enemy_setBattler = Sprite_Enemy.prototype.setBattler;
   Sprite_Enemy.prototype.setBattler = function(battler) {
     _Sprite_Enemy_setBattler.call(this, battler);
-    if($dataEnemies[battler._enemyId].meta.BigEnemy){
-      const name = this._enemy.battlerName();
+    if (battler.enemy().meta.BigEnemy) {
       this._bigEnemy = battler.enemy().meta.BigEnemy;
-      if ($gameSystem.isSideView()) {
-        bitmap = ImageManager.loadSvEnemy(name);
-      } else {
-        bitmap = ImageManager.loadEnemy(name);
-      }
       if (this._bigEnemy) {
-        if (bitmap && !bitmap.isReady()) {
-          bitmap.addLoadListener(this.setBigEnemy.bind(this, bitmap, battler));
+        const name = this._enemy.battlerName();
+        if ($gameSystem.isSideView()) {
+          ImageManager.loadSvEnemy(name);
         } else {
-          this.setBigEnemy(bitmap, battler);
+          ImageManager.loadEnemy(name);
+        }
+        if (this.bitmap && !this.bitmap.isReady()) {
+          this.bitmap.addLoadListener(this.setBigEnemy.bind(this));
         }
       }
     }
   };
 
-  Sprite_Enemy.prototype.setBigEnemy = function(bitmap, battler) {
-    this.scale.x = Graphics.width / bitmap.width;
-    this._homeX = Graphics.boxWidth / 2;
-    let height = 0;
-    if(this._bigEnemy === 'OriginalRatio') {
-      this.scale.y = this.scale.x;
-      height = Math.floor((Graphics.height - bitmap.height * this.scale.y) / 2);
+  Sprite_Enemy.prototype.setBigEnemy = function() {
+    const bitmap = this.bitmap;
+    if (this._bigEnemy === "UnderPosition") {
+      this._homeY = Graphics.height + 20;
     } else {
-      this.scale.y = Graphics.height / bitmap.height;
+      this.scale.x = Graphics.width / bitmap.width;
+      this._homeX = Graphics.boxWidth / 2;
+      let height = 0;
+      if (this._bigEnemy === 'OriginalRatio') {
+        this.scale.y = this.scale.x;
+        height = Math.floor((Graphics.height - bitmap.height * this.scale.y) / 2);
+      } else {
+        this.scale.y = Graphics.height / bitmap.height;
+      }
+      this._homeY = Math.floor((Graphics.height - Graphics.boxHeight) / 2 + Graphics.boxHeight) + 24 - height;
     }
-    this._homeY = Math.floor((Graphics.height - Graphics.boxHeight) / 2 + Graphics.boxHeight) + 24 - height;
-    this._homeY += battler.enemy().meta.BigEnemyY ? Number(battler.enemy().meta.BigEnemyY) : 0;
+    this._homeY += this._enemy.enemy().meta.BigEnemyY ? Number(this._enemy.enemy().meta.BigEnemyY) : 0;
     this.updatePosition();
   };
 
   const _Sprite_Enemy_updateStateSprite = Sprite_Enemy.prototype.updateStateSprite;
   Sprite_Enemy.prototype.updateStateSprite = function() {
     _Sprite_Enemy_updateStateSprite.call(this);
-    if(this._bigEnemy){
+    if(this._bigEnemy && this._bigEnemy !== "UnderPosition"){
       this._stateIconSprite.y = (40 - this.y) / this.scale.y;
       this._stateIconSprite.scale.x = 1 / this.scale.x;
       this._stateIconSprite.scale.y = 1 / this.scale.y;
@@ -102,9 +116,27 @@ Imported.NUUN_BigEnemy = true;
   Sprite_Enemy.prototype.damageOffsetY = function() {
     let y = _Sprite_Enemy_damageOffsetY.call(this);
     if(this._bigEnemy){
-      y -= Math.floor(this._bigEnemy === 'OriginalRatio' ? this.bitmap.height * this.scale.y / 2 : Graphics.boxHeight / 2);
+      if (this._bigEnemy === "OriginalRatio") {
+        y -= this.bitmap.height * this.scale.y / 2
+      } else if (this._bigEnemy === "UnderPosition") {
+        y -= this.bitmap.height / 2;
+      } else {
+        y -= Graphics.boxHeight / 2
+      }
     }
     return y;
+  };
+
+  const _Sprite_Enemy_loadBitmap = Sprite_Enemy.prototype.loadBitmap;
+  Sprite_Enemy.prototype.loadBitmap = function(name) {
+    _Sprite_Enemy_loadBitmap.call(this, name);
+    if (this._bigEnemy) {
+      if (this.bitmap && !this.bitmap.isReady()) {
+        this.bitmap.addLoadListener(this.setBigEnemy.bind(this));
+      } else {
+        this.setBigEnemy();
+      }
+    }
   };
 
 })();

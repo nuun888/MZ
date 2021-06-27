db444b3fe6c2daa3cc5cb1b112125afc0b86e333
nuun888 <70774024+nuun888@users.noreply.github.com>
2021-06-27 13:32:21 +0900

    Add files via upload

--- NUUN_LevelUnlimited.js
+++ NUUN_LevelUnlimited.js
@@ -6,31 +6,35 @@
  * http://opensource.org/licenses/mit-license.php
  * -------------------------------------------------------------------------------------
  * 
- * 更新履歴
- * 2020/12/12 Ver 1.0.0
- * 初版
- * 
- * 2020/12/12 Ver 1.0.1
- *  レベル１００以上のステータスの計算が間違っていたのを修正。
  */ 
 /*:
  * @target MZ
  * @plugindesc レベル上限限界突破プラグイン
  * @author NUUN
+ * @version 1.2.0
  * 
  * @help
  * 最大レベルを１００以上に設定できます。
  * アクターのメモ欄に<MaxLevel:[最大レベル]>を記入してください。
  * <MaxLevel:200> アクターの最大レベルは２００になります。
+ * <StartLevel:120> 初期レベルを設定します。設定例では初期レベルが１２０になります。
  * 
  * レベル１００以上でスキルを習得させる場合は職業の習得スキルのメモ欄に
  * <LearnSkill:[習得レベル]>を記入します。デフォルトの習得レベルは
  * 無視されます。
  * <LearnSkill:105> レベル１０５にレベルアップした時にスキルを習得します。
  * 
- * 
- * 利用規
+ * 利用規約
  * このプラグインはMITライセンスで配布しています。
+ * 
+ * 更新履歴
+ * 2021/6/27 Ver 1.2.0
+ * 初期レベルをレベル１００以上に設定できる機能を追加。
+ * 2020/12/12 Ver 1.0.1
+ * レベル１００以上のステータスの計算が間違っていたのを修正。
+ * 2020/12/12 Ver 1.0.0
+ * 初版
+ * 
  * 
  */
 var Imported = Imported || {};
@@ -38,6 +42,21 @@ Imported.NUUN_LevelUnlimited = true;
 
 (() => {
 const parameters = PluginManager.parameters('NUUN_LevelUnlimited');
+
+const _Game_Actor_setup = Game_Actor.prototype.setup;
+Game_Actor.prototype.setup = function(actorId) {
+  _Game_Actor_setup.call(this, actorId);
+  const actor = $dataActors[actorId];
+  if (actor.meta.StartLevel) {
+    this._level = Number(actor.meta.StartLevel) || actor.initialLevel;
+    this.initImages();
+    this.initExp();
+    this.initSkills();
+    this.initEquips(actor.equips);
+    this.clearParamPlus();
+    this.recoverAll();
+  }
+};
 
 const _Game_Actor_paramBase = Game_Actor.prototype.paramBase;
 Game_Actor.prototype.paramBase = function(paramId) {
@@ -62,7 +81,7 @@ Game_Actor.prototype.maxLevel = function() {
 Game_Actor.prototype.initSkills = function() {
   this._skills = [];
   for (const learning of this.currentClass().learnings) {
-    let learnLv = Number(this.learnSkillNote(learning));
+    let learnLv = this.learnSkillNote(learning);
     learnLv = learnLv > 0 ? learnLv : learning.level;
     if (learnLv <= this._level) {
       this.learnSkill(learning.skillId);
@@ -73,7 +92,7 @@ Game_Actor.prototype.initSkills = function() {
 Game_Actor.prototype.levelUp = function() {
   this._level++;
   for (const learning of this.currentClass().learnings) {
-    let learnLv = Number(this.learnSkillNote(learning));
+    let learnLv = this.learnSkillNote(learning);
     learnLv = learnLv > 0 ? learnLv : learning.level;
     if (learnLv === this._level) {
       this.learnSkill(learning.skillId);

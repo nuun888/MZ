3e12fbd10196bea41c58bea6e9f3e2cbbfff3167
nuun888 <70774024+nuun888@users.noreply.github.com>
2021-07-11 19:45:21 +0900

    Add files via upload

--- NUUN_MaxItem.js
+++ NUUN_MaxItem.js
@@ -11,12 +11,13 @@
  * @target MZ
  * @plugindesc アイテム最大所持数変更
  * @author NUUN
- * @version 1.0.0
+ * @version 1.1.0
  * 
  * @help
  * アイテムの所持数を個別に設定します。
  * アイテム、武器、防具のメモ欄に以下の書式を記入します。
  * <MaxItems:10> そのアイテムは最大１０個までしか持つことはできません。
+ * <NoItemNum> アイテムの個数を表示しません。
  * 
  * またデフォルトの最大所持数も設定することもできます。
  * 最大所持数はメモ欄に設定した最大所持数が優先されます。
@@ -27,32 +28,46 @@
  * 以下の機能はNUUN_Base Ver1.1.3以降、NUUN_ItemCategoryが必要です。
  * NUUN_ItemCategory（アイテムカテゴリーカスタマイズ）と併用している場合、そのカテゴリー内の
  * アイテムのデフォルトの愛大所持数を設定できます。なおアイテムのみ有効です。
- * カテゴリーキーにkeyItemと記入した場合は大事なもの（CategoryTypeタグを記入していない）のアイテムのデフォルトの最大所持数が変更されます。
+ * <CategoryType:[key]> に設定したカテゴリーのアイテムのデフォルトの最大所持数が変更されます。
  * 
+ * 予約カテゴリーキー
+ * keyItem：大事なもの
+ * HiddenItemA：隠しアイテムA
+ * HiddenItemB：隠しアイテムB
+ * カテゴリーキーにkeyItemと記入した場合は大事なもの（CategoryTypeタグを記入していない）のアイテムのデフォルトの最大所持数が変更されます。
  * 
  * 利用規約
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2021/7/11 Ver 1.1.0
+ * 特定のアイテム個数を非表示にする機能を追加。
+ * 所持個数/最大個数で表示させる機能を追加。
+ * 2021/6/21 Ver 1.0.1
+ * 隠しアイテムに対応。（要NUUN_ItemCategory）
  * 2021/6/20 Ver 1.0.0
+ * 初版
  * 
  * @param DefaultMaxItem
  * @desc デフォルトの最大アイテム所持数
  * @text デフォルトの最大アイテム所持数を設定します。
  * @type number
  * @default 99
+ * @min 1
  * 
  * @param DefaultMaxWeapon
  * @desc デフォルトの最大武器所持数
  * @text デフォルトの最大武器所持数を設定します。
  * @type number
  * @default 99
+ * @min 1
  * 
  * @param DefaultMaxArmor
  * @desc デフォルトの最大防具所持数
  * @text デフォルトの最大防具所持数を設定します。
  * @type number
  * @default 99
+ * @min 1
  * 
  * @param CustomDefault
  * @desc アイテムカテゴリーカスタマイズで設定したカテゴリーアイテムのデフォルト最大所持数(要NUUN_ItemCategory)
@@ -65,6 +80,16 @@
  * @text 個数の桁数を設定します。例　0が３つで３桁分(000)
  * @type string
  * @default 00
+ * 
+ * @param ItemNumMode
+ * @desc 表示形式。
+ * @text 表示形式。
+ * @type select
+ * @option 所持個数のみ
+ * @value 0
+ * @option 所持個数/所持最大数
+ * @value 1
+ * @default 0
  * 
  */
 /*~struct~CustomDefaultList:
@@ -80,6 +105,7 @@
  * @text カテゴリー最大所持数
  * @type number
  * @default 99
+ * @min 1
  *
  */
 
@@ -89,7 +115,17 @@
   const defaultMaxWeapon = Number(parameters['DefaultMaxWeapon'] || 99);
   const defaultMaxArmor = Number(parameters['DefaultMaxArmor'] || 99);
   const numberDigits = String(parameters['NumberDigits'] || '00');
+  const ItemNumMode = Number(parameters['ItemNumMode'] || 0);
   const CustomDefault = (NUUN_Base_Ver >= 113 ? (DataManager.nuun_structureData(parameters['CustomDefault'])) : null) || [];
+  let itemData = null;
+
+  function setItemData(item) {
+    itemData = item;
+  }
+
+  function getItemData() {
+    return itemData;
+  }
 
   Game_Party.prototype.maxItems = function(item) {//再定義
     if (item.meta.MaxItems) {
@@ -118,18 +154,41 @@
   };
 
   Game_Party.prototype.getCategoryType = function(item) {
-    return item.meta.CategoryType ? item.meta.CategoryType : (item.itypeId === 2 ? "keyItem" : null);
+    return item.meta.CategoryType ? item.meta.CategoryType : this.getCategoryKeyItem(item);
+  };
+
+  Game_Party.prototype.getCategoryKeyItem = function(item) {
+    if (item.itypeId === 2) {
+      return "keyItem";
+    } else if (item.itypeId === 3) {
+      return "HiddenItemA";
+    } else if (item.itypeId === 4) {
+      return "HiddenItemB";
+    }
+    return null;
   };
 
   Window_ItemList.prototype.numberWidth = function() {//再定義
-    return this.textWidth(numberDigits + '0');
+    return getItemData().meta.NoItemNum ? 0 : this.textWidth(numberDigits + '0');
   };
 
+  const _Window_ItemList_drawItem = Window_ItemList.prototype.drawItem;
+  Window_ItemList.prototype.drawItem = function(index) {
+    setItemData(this.itemAt(index));
+    _Window_ItemList_drawItem.call(this, index);
+};
+
   Window_ItemList.prototype.drawItemNumber = function(item, x, y, width) {//再定義
-    if (this.needsNumber()) {
+    if (this.needsNumber() && !item.meta.NoItemNum) {
         const numberWidth = this.textWidth(numberDigits);
         this.drawText(":", x, y, width - numberWidth, "right");
-        this.drawText($gameParty.numItems(item), x + width - numberWidth, y, numberWidth, "right");
+        let text = null;
+        if (ItemNumMode === 0) {
+          text = $gameParty.numItems(item);
+        } else {
+          text = $gameParty.numItems(item) +"/"+ $gameParty.maxItems(item);
+        }
+        this.drawText(text, x + width - numberWidth, y, numberWidth, "right");
     }
   };
 })();

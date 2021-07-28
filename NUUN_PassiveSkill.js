b187b9c77d682af43763d7b4f87c1ec10c1ad96e
nuun888 <70774024+nuun888@users.noreply.github.com>
2021-07-28 21:12:03 +0900

    Add files via upload

--- NUUN_PassiveSkill.js
+++ NUUN_PassiveSkill.js
@@ -10,7 +10,7 @@
  * @target MZ
  * @plugindesc パッシブスキル
  * @author NUUN
- * @version 1.0.0
+ * @version 1.1.0
  * @base NUUN_Base
  * 
  * @help
@@ -36,19 +36,25 @@
  * 対象:MP 上限値:0　下限値:80 ＭＰが８０％以上の時に反映します。
  * 対象:TP 上限値:0　下限値:100 ＴＰが１００％の時に反映します。
  * 対象:State ステートID:6 ステート６番のステートに掛かっている時に反映します。
+ * 対象:StateR ステートID:5 ステート５番のステートに掛掛かっていない時に反映します。
  * 対象:Turn 上限値:0　下限値:10 １０ターン目以降に反映します。
  * 対象:Turn 上限値:3　下限値:0 ３ターン目まで反映します。
  * 対象:GVal ゲーム変数:5 上限値:30　下限値:10 ゲーム変数５番が１０以上３０以下の時に反映します。
  * 対象:GSwc ゲーム変数スイッチ:5 ゲームスイッチがtrueの時に反映します。
- * 
- * 武器のメモ欄に記入するタイプのプラグインであればそのプラグインの効果によるパッシブスキルも製作できます。
+ * 対象:Equip 武器タイプ:10 武器タイプ10番の武器を装備しているに反映します。武器タイプを設定している場合は防具タイプの設定は無視されます。
+ * 対象:Equip 防具タイプ:8 防具タイプ8番の武器を装備しているに反映します。
  * 
  * このプラグインはNUUN_Baseが必要です。
+ * 
  * 
  * 利用規約
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2021/7/28 Ver.1.1.0
+ * ステートに掛かっていない時に反映させる機能を追加。
+ * 特定の武器、防具タイプを装備している時のみ反映させる機能を追加。
+ * 特定の状況でステータスの表示がされていなかった問題を修正。
  * 2021/7/27 Ver.1.0.0
  * 初版
  * 
@@ -77,8 +83,12 @@
  * @value 'MP'
  * @option TP
  * @value 'TP'
- * @option ステート
+ * @option ステート適用
  * @value 'State'
+ * @option ステート除外
+ * @value 'StateR'
+ * @option 装備タイプ
+ * @value 'Equip'
  * @option ターン
  * @value 'Turn'
  * @option ゲーム変数
@@ -99,7 +109,19 @@
  * @type number
  * @default 0
  * 
- * @param StateImgId
+ * @param EquipWeapon
+ * @text 武器タイプ
+ * @desc 武器タイプを指定します。指定した武器タイプを装備している時に反映します。(武器)
+ * @type number
+ * @default 0
+ * 
+ * @param EquipArmor
+ * @text 防具タイプ
+ * @desc 防具タイプを指定します。指定した防具タイプを装備している時に反映します。(武器)
+ * @type number
+ * @default 0
+ * 
+ * @param StateId
  * @text ステートID
  * @desc ステートIDを指定します。指定した状態に掛かっている時に反映します。
  * @type state
@@ -107,13 +129,13 @@
  * 
  * @param VariableId
  * @text ゲーム変数
- * @desc ゲーム変数を指定します。[上限値][下限値]　下限値0で[上限値]以上
+ * @desc ゲーム変数を指定します。[上限値][下限値]　下限値0で[上限値]以上（ゲーム変数）
  * @type variable
  * @default 0
  * 
  * @param SwitchId
  * @text ゲームスイッチ
- * @desc ゲームスイッチを指定します。trueの時に反映します。
+ * @desc ゲームスイッチを指定します。trueの時に反映します。（スイッチ）
  * @type switch
  * @default 0
  * 
@@ -128,14 +150,21 @@ Imported.NUUN_PassiveSkill = true;
 
   const _Game_Actor_refresh = Game_Actor.prototype.refresh;
   Game_Actor.prototype.refresh = function() {
-    _Game_Actor_refresh.call(this);
     this.passiveRefresh();
+    _Game_Actor_refresh.call(this);
   };
 
   const _Game_Actor_initMembers = Game_Actor.prototype.initMembers;
   Game_Actor.prototype.initMembers = function() {
     _Game_Actor_initMembers.call(this);
     this._passiveWeapon = [];
+  };
+
+  const _Game_Actor_setup =Game_Actor.prototype.setup;
+  Game_Actor.prototype.setup = function(actorId) {
+    _Game_Actor_setup.call(this, actorId);
+    this.passiveRefresh();
+    this.recoverAll();
   };
 
   Game_Actor.prototype.passiveRefresh = function() {
@@ -179,7 +208,11 @@ Imported.NUUN_PassiveSkill = true;
       case 'TP':
         return this.tp >= this.maxTp() * list.DwLimit / 100 && (list.UpLimit > 0 ? this.tp <= this.maxTp() * list.UpLimit / 100 : true);
       case 'State':
-        return this._states.find(id => id === list.StateImgId);
+        return this._states.find(id => id === list.StateId);
+      case 'StateR':
+        return !(this._states.some(id => id === list.StateId));
+      case 'Equip':
+        return list.EquipWeapon > 0 ? this.isEquippedWeaponType(list.EquipWeapon) : this.isEquippedArmorType(list.EquipWeapon);
       case 'Turn':
         return this.turnCount() >= list.DwLimit && (list.UpLimit > 0 ? this.turnCount() <= list.UpLimit : true);
       case 'GVal':
@@ -187,7 +220,18 @@ Imported.NUUN_PassiveSkill = true;
          (list.UpLimit > 0 ? $gameVariables.value(list.VariableId) <= list.UpLimit : true);
       case 'GSwc':
         return $gameSwitches.value(list.SwitchId);
+      case 'Eval':
+        return new Function(String(list.EvalData));
     }
+    return false;
+  };
+
+  Game_Actor.prototype.isEquippedWeaponType = function(type) {
+    return this.equips().some(equip => equip && equip.wtypeId === type);
+  };
+
+  Game_Actor.prototype.isEquippedArmorType = function(type) {
+    return this.equips().some(equip => equip && equip.atypeId === type);
   };
 
   Game_Actor.prototype.setPassiveSkill = function() {
@@ -247,6 +291,12 @@ Imported.NUUN_PassiveSkill = true;
 
   Game_Party.prototype.passiveRefresh = function() {
     this.allMembers().forEach(actor => actor.passiveRefresh());
+  };
+
+  const _BattleManager_setup = BattleManager.setup;
+  BattleManager.setup = function(troopId, canEscape, canLose) {
+    _BattleManager_setup.call(this, troopId, canEscape, canLose);
+    $gameParty.passiveRefresh();
   };
 
 })();

f4efb3ae1fbc028cf25a459c294becafc8bc7f83
nuun888 <70774024+nuun888@users.noreply.github.com>
2021-09-08 22:39:33 +0900

    Add files via upload

--- NUUN_ActorFixed.js
+++ NUUN_ActorFixed.js
@@ -10,7 +10,7 @@
  * @target MZ
  * @plugindesc アクター並び替え固定
  * @author NUUN
- * @version 1.1.0
+ * @version 1.1.1
  * 
  * @help
  * アクターの並び替えを固定します。
@@ -19,6 +19,8 @@
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2021/9/8 Ver.1.1.1
+ * 固定アクターを戦闘メンバー内でも入れ替えできるように修正。
  * 2021/8/25 Ver.1.1.0
  * 固定したメンバーを控えメンバーに移動させない機能を追加。
  * 2021/8/17 Ver.1.0.0
@@ -46,8 +48,8 @@
  * 
  * 
  * @param ActorFixedMovable
- * @text 待機固定アクター移動可
- * @desc 待機メンバーの固定アクターの戦闘メンバーへの移動を許可します。
+ * @text 固定アクター戦闘メンバーへの移動可
+ * @desc 固定アクターの戦闘メンバーへの移動を許可します。
  * @type boolean
  * @default false
  * @parent ActorFixedSetting
@@ -97,32 +99,40 @@ Game_Actor.prototype.isFixedMovable = function() {
 };
 
 Game_Actor.prototype.isFixed = function() {
-  return (this._fixed === undefined ? false : this._fixed) && !this.isFixedMovable();
+  return (this._fixed === undefined ? false : this._fixed);
 };
 
 Game_Actor.prototype.getFixed = function() {
   return (this._fixed === undefined ? false : this._fixed);
 };
 
-const _Game_Actor_isFormationChangeOk = Game_Actor.prototype.isFormationChangeOk;
-Game_Actor.prototype.isFormationChangeOk = function() {
-  return _Game_Actor_isFormationChangeOk.call(this) && !this.isFixed();
+Game_Actor.prototype.setFixedMovable_org = function() {
+  this.setFixedMovable(ActorFixedMovable);
 };
 
-Game_Actor.prototype.setFixedMovable_pp = function() {
-  this.setFixedMovable(ActorFixedMovable);
+const _Game_Actor_isFormationChangeOk = Game_Actor.prototype.isFormationChangeOk;
+Game_Actor.prototype.isFormationChangeOk = function() {
+  return _Game_Actor_isFormationChangeOk.call(this) && !this.isFixed() || this.isFixedMovable();
 };
 
 const _Window_MenuStatus_isCurrentItemEnabled = Window_MenuStatus.prototype.isCurrentItemEnabled;
 Window_MenuStatus.prototype.isCurrentItemEnabled = function() {
-  const actor = this.actor(this.index());
   if (this._formationMode) {
-    if (this._pendingIndex < $gameParty.maxBattleMembers() && this.index() >= $gameParty.maxBattleMembers()) {
+    const actor = this.actor(this.index());
+    let pendingActor = true;
+    if (this._pendingIndex < 0) {
+      actor.setFixedMovable(ActorFixedMovable);
+    } else if (this._pendingIndex >= $gameParty.maxBattleMembers()) {
+      actor.setFixedMovable(false);
+    } else if (this.index() < $gameParty.maxBattleMembers()) {
       actor.setFixedMovable(ActorFixedMovable);
     } else {
       actor.setFixedMovable(false);
+      pendingActor = !this.actor(this._pendingIndex).isFixed();
     }
+    return _Window_MenuStatus_isCurrentItemEnabled.call(this) && pendingActor;
   }
   return _Window_MenuStatus_isCurrentItemEnabled.call(this);
 };
+
 })();

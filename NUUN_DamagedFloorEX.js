c24e1855eca6c3eafd40afdd3314d5c0e0accfae
nuun888 <70774024+nuun888@users.noreply.github.com>
2021-10-31 19:00:20 +0900

    Add files via upload

--- NUUN_DamagedFloorEX.js
+++ NUUN_DamagedFloorEX.js
@@ -18,6 +18,10 @@
  * @help
  * ダメージ床のダメージ時の処理を拡張します。
  * リージョン設定を0にすることでリージョンを指定していないマップタイルで適用されます。
+ * リージョンと地形タグを両方指定している場合はどちらかが一致したときに適用されます。
+ * 床ダメージ設定の優先度はリストの上の設定から判定し一致した条件が適用されます。
+ * リージョン、地形タグを設定した条件は上のほうに設定してください。
+ * フラッシュ設定はリスト５番の数値を0と入力することでフラッシュされません。
  * 
  * 床ダメージ値は評価式が使用できます。
  * a：アクター
@@ -27,6 +31,8 @@
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2021/10/31 Ver.1.0.3
+ * 地形タグ、フラッシュを設定できる機能を追加。
  * 2021/10/24 Ver.1.0.2
  * 床ダメージを評価式が使用できるように変更。
  * 2021/10/24 Ver.1.0.1
@@ -84,6 +90,17 @@
  * @default 50
  * @parent DefaultSE
  * 
+ * @param Flash
+ * @text フラッシュ設定
+ * @default ------------------------------
+ * 
+ * @param DefaultFlashColor
+ * @text フラッシュ色
+ * @desc フラッシュの色。（1:赤、2:緑、3:青、4:グレー、5:フレーム数）フレーム数を0にすることでフラッシュしません。
+ * @type string[]
+ * @default ["255","0","0","128","8"]
+ * @parent Flash
+ * 
  */
 /*~struct~DamagedFloorData:
  * 
@@ -109,6 +126,14 @@
  * @type number
  * @default 0
  * 
+ * @param TerrainId
+ * @text 地形タグID
+ * @desc 地形タグID
+ * @type number
+ * @default -1
+ * @min -1
+ * @max 7
+ * 
  * @param Damage
  * @text ダメージ
  * @desc 床ダメージ時のダメージ（a：アクター）
@@ -132,6 +157,8 @@
  * @type number
  * @default 90
  * @parent SE
+ * @min 0
+ * @max 9999
  * 
  * @param pitch
  * @text ピッチ
@@ -147,6 +174,17 @@
  * @default 50
  * @parent SE
  * 
+ * @param Flash
+ * @text フラッシュ設定
+ * @default ------------------------------
+ * 
+ * @param FlashColor
+ * @text フラッシュ色
+ * @desc フラッシュの色。（1:赤、2:緑、3:青、4:グレー、5:フレーム数）フレーム数を0にすることでフラッシュしません
+ * @type string[]
+ * @default 
+ * @parent Flash
+ * 
  */
 var Imported = Imported || {};
 Imported.NUUN_DamagedFloorEX = true;
@@ -154,15 +192,14 @@ Imported.NUUN_DamagedFloorEX = true;
 (() => {
 const parameters = PluginManager.parameters('NUUN_DamagedFloorEX');
 const DamagedFloorList = (NUUN_Base_Ver >= 113 ? (DataManager.nuun_structureData(parameters['DamagedFloorList'])) : null) || [];
-const DamagedFloorFlash = eval(parameters['DamagedFloorFlash'] || 'false');
+const DefaultFlashColor = eval(parameters['DefaultFlashColor']);
 const DefaultDamage = String(parameters['DefaultDamage'] || '10');
 const DefaultDamagedFloorSE = String(parameters['DefaultDamagedFloorSE'] || '');
 const DefaultVolume = Number(parameters['DefaultVolume'] || 90);
 const DefaultPitch = Number(parameters['DefaultPitch'] || 100);
 const DefaultPan = Number(parameters['DefaultPan'] || 50);
-//フラッシュ指定　地形タグ　床ダメージ独自処理
 let onMapFloorDamage = false;
-let floorDamageRefresh = false;
+let FlashColor = [];
 
 const _Game_Actor_executeFloorDamage = Game_Actor.prototype.executeFloorDamage;
 Game_Actor.prototype.executeFloorDamage = function() {
@@ -174,13 +211,8 @@ Game_Actor.prototype.executeFloorDamage = function() {
 const _Game_Actor_performMapDamage = Game_Actor.prototype.performMapDamage;
 Game_Actor.prototype.performMapDamage = function() {
   if (onMapFloorDamage) {
-    if (!$gameParty.inBattle() && floorDamageRefresh) {
-      floorDamageRefresh = false;
-      if (DamagedFloorFlash) {
-        _Game_Actor_performMapDamage.call(this);
-      }
+    _Game_Actor_performMapDamage.call(this);
     $gameMap.getDamagedFloorListData();
-    }
   } else {
     _Game_Actor_performMapDamage.call(this);
   }
@@ -195,11 +227,31 @@ Game_Actor.prototype.basicFloorDamage = function() {
     const x = $gamePlayer._x;
     const y = $gamePlayer._y;
     const regionId = $gameMap.regionId(x, y);
-    const damagedFloorData = DamagedFloorList[$gameMap._damagedFloorId].DamagedFloorRegion || [];
-    const mainData = damagedFloorData.find(data => data.RegionId === regionId);
+    const Terrain = $gameMap.terrainTag(x, y);
+    const damagedFloorData = DamagedFloorList[damagedFloorId].DamagedFloorRegion || [];
+    const mainData = damagedFloorData.find(data => data.RegionId === regionId || data.TerrainId === Terrain);
+    if (mainData.FlashColor) {
+      setFlashData(mainData.FlashColor);
+    } else {
+      setFlashData(DefaultFlashColor);
+    }
     return mainData && mainData.Damage ? eval(mainData.Damage) : DefaultDamage ? eval(DefaultDamage) : coreDamage;
   } else {
+    setFlashData(DefaultFlashColor);
     return DefaultDamage ? eval(DefaultDamage) : coreDamage;
+  }
+};
+
+
+const _Game_Screen_startFlashForDamage = Game_Screen.prototype.startFlashForDamage;
+Game_Screen.prototype.startFlashForDamage = function() {
+  if (onMapFloorDamage) {
+    if (FlashColor[4] > 0) {
+      this.startFlash([FlashColor[0], FlashColor[1], FlashColor[2], FlashColor[3]], FlashColor[4]);
+    }
+    FlashColor = [];
+  } else {
+    _Game_Screen_startFlashForDamage.call(this);
   }
 };
 
@@ -233,7 +285,8 @@ Game_Map.prototype.getDamagedFloorListData = function() {
     const y = $gamePlayer._y;
     const regionId = this.regionId(x, y);
     const damagedFloorData = DamagedFloorList[this._damagedFloorId].DamagedFloorRegion || [];
-    const mainData = damagedFloorData.find(data => data.RegionId === regionId);
+    const Terrain = $gameMap.terrainTag(x, y);
+    const mainData = damagedFloorData.find(data => data.RegionId === regionId || data.TerrainId === Terrain);
     if (mainData && mainData.DamagedFloorSE) {
       AudioManager.playSe({"name":mainData.DamagedFloorSE,"volume":mainData.volume,"pitch":mainData.pitch,"pan":mainData.pan});
     } else if (DefaultDamagedFloorSE) {
@@ -244,6 +297,14 @@ Game_Map.prototype.getDamagedFloorListData = function() {
       AudioManager.playSe({"name":DefaultDamagedFloorSE,"volume":DefaultVolume,"pitch":DefaultPitch,"pan":DefaultPan});
     }
   }
+};
+
+function setFlashData(data) {
+  FlashColor[0] = parseInt(data[0]).clamp(0, 255);
+  FlashColor[1] = parseInt(data[1]).clamp(0, 255);
+  FlashColor[2] = parseInt(data[2]).clamp(0, 255);
+  FlashColor[3] = parseInt(data[3]).clamp(0, 255);
+  FlashColor[4] = parseInt(data[4]);
 };
 
 })();

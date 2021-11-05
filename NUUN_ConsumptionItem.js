80e54504bc97a69c0832bf81654b3aa1a02470cf
nuun888 <70774024+nuun888@users.noreply.github.com>
2021-11-06 07:48:38 +0900

    Add files via upload

--- NUUN_ConsumptionItem.js
+++ NUUN_ConsumptionItem.js
@@ -11,10 +11,13 @@
  * @target MZ
  * @plugindesc  アイテム消耗率
  * @author NUUN
- * @version 1.1.0
+ * @version 1.1.1
  * 
  * @help
  * アイテムに一定の確率で消費するアイテムを作ることが出来ます。
+ * 消耗率を軽減できる特徴を設定できます。
+ * なお、マップ上ではパーティメンバーの中で一番軽減率の高い数値で判定します。
+ * 戦闘中では使用者の軽減率で判定します。
  * 
  * アイテムのメモ欄に<ConsumptionRate:[rate]>を記入します。
  * [rate]:確率
@@ -32,11 +35,48 @@
  * [pitch]:ピッチ
  * [pan]:位相
  * 
+ * 
+ * 特徴を持つメモ欄
+ * <ConsumptionRatio:[ratio]> 消耗する確率を軽減します。
+ * [ratio]：補正消耗率
+ * <ConsumptionRatio:70> 消耗率が70%されます。消耗率50%のアイテムは35%の確率で消耗します。
+ * このタグが有効なアイテムはConsumptionRateのタグが設定されたアイテムのみ適用されます。
+ * 
+ * <NoConsumptionRatio>
+ * このアイテムには消耗率の軽減が適用されません。
+ * 
  * 更新履歴
+ * 2021/11/6 Ver.1.1.1
+ * 消耗時のデフォルトのSEを設定出来る項目を追加。
+ * 消耗率を軽減出来る特徴を設定できる機能を追加。
  * 2021/11/3 Ver.1.1.0
  * メッセージのフォーマットを変更。
  * 2020/12/31 Ver.1.0.0
  * 初版
+ * 
+ * @param ConsumptionItemSE
+ * @text 消耗時SE
+ * @desc 消耗率が設定してあるアイテムが消耗したときのSE
+ * @type file
+ * @dir audio/se/
+ * 
+ * @param volume
+ * @text 音量
+ * @desc 音量。
+ * @type number
+ * @default 90
+ * 
+ * @param pitch
+ * @text ピッチ
+ * @desc ピッチ。
+ * @type number
+ * @default 100
+ * 
+ * @param pan
+ * @text 位相
+ * @desc 位相。
+ * @type number
+ * @default 50
  * 
  */
 var Imported = Imported || {};
@@ -45,6 +85,17 @@ Imported.NUUN_ConsumptionItem = true;
 (() => {
 const parameters = PluginManager.parameters('NUUN_ConsumptionItem');
 const ConsumptionNoLog = eval(parameters['ConsumptionNoLog'] || 'false');
+const ConsumptionItemSE = String(parameters['ConsumptionItemSE'] || '');
+const volume = Number(parameters['volume'] || 90);
+const pitch = Number(parameters['pitch'] || 100);
+const pan = Number(parameters['pan'] || 50);
+let useActor = null;
+
+const _Game_Battler_consumeItem = Game_Battler.prototype.consumeItem;
+Game_Battler.prototype.consumeItem = function(item) {
+  useActor = this;
+  _Game_Battler_consumeItem.call(this, item);
+};
 
 const _Game_Party_consumeItem = Game_Party.prototype.consumeItem;
 Game_Party.prototype.consumeItem = function(item) {
@@ -54,13 +105,17 @@ Game_Party.prototype.consumeItem = function(item) {
 };
 
 Game_Party.prototype.consumeRate = function(item) {
-  const rate = Math.min(item.meta.ConsumptionRate ? item.meta.ConsumptionRate : 100, 100);
-  const reslit = Math.floor(Math.random() * 100 < rate);
-  if (reslit) {
-    this.consumptionMessage(item);
-    this.consumptionSe(item);
+  if (item.meta.ConsumptionRate) {
+    const rate = Math.min(item.meta.ConsumptionRate ? item.meta.ConsumptionRate : 100, 100);
+    const reslit = Math.floor(Math.random() * 100 < (item.meta.NoConsumptionRatio ? rate : this.consumptionItemRatio(rate)));
+    if (reslit) {
+      this.consumptionMessage(item);
+      this.consumptionSe(item);
+    }
+    return reslit;
+  } else {
+    return true;
   }
-  return reslit;
 };
 
 Game_Party.prototype.consumptionMessage = function(item) {
@@ -75,7 +130,39 @@ Game_Party.prototype.consumptionSe = function(item) {
   if (item.meta.ConsumptionSE) {
     const date = item.meta.ConsumptionSE.split(',');
     AudioManager.playSe({"name":date[0],"volume":date[1],"pitch":date[2],"pan":date[3]});
+  } else if (ConsumptionItemSE) {
+    AudioManager.playSe({"name":ConsumptionItemSE,"volume":volume,"pitch":pitch,"pan":pan});
   }
+};
+
+Game_Party.prototype.consumptionItemRatio = function(rate) {
+  return rate * (this.inBattle() ? this.consumptionRatioInBattle() : this.consumptionRatioMembers());
+};
+
+Game_Party.prototype.consumptionRatioInBattle = function() {
+  const actor = useActor;
+  useActor = null;
+  return this.consumptionRatio(actor);
+};
+
+Game_Party.prototype.consumptionRatioMembers = function(item) {
+  return this.battleMembers().reduce((r, member) => {
+    const rate = this.consumptionRatio(member);
+    if (r > rate) {
+      r = rate;
+    }
+    return r;
+  }, 1.0);
+};
+
+Game_Party.prototype.consumptionRatio = function(member) {
+  return member.traitObjects().reduce((r, obj) => {
+    if (obj.meta.ConsumptionRatio) {
+      return r * obj.meta.ConsumptionRatio / 100;
+    } else {
+      return r;
+    }
+  }, 1.0);
 };
 
 const _BattleManager_initMembers = BattleManager.initMembers;

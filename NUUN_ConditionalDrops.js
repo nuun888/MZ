989ce0c86efbe354a5aa10154d12210f6c28382d
nuun888 <70774024+nuun888@users.noreply.github.com>
2021-11-12 23:55:27 +0900

    Add files via upload

--- NUUN_ConditionalDrops.js
+++ NUUN_ConditionalDrops.js
@@ -10,29 +10,28 @@
  * @target MZ
  * @plugindesc 条件付きドロップ
  * @author NUUN
- * @version 1.0.1
+ * @version 1.0.2
  * @base NUUN_ConditionsBase
  * 
  * @help
  * 
  * 敵のメモ欄
- * <CondDropItem:[item],[id],[rate],[condNameTag]>
+ * <CondDropItem:[item],[id],[rate],[condNameTag],[CondMode]>
  * [item]:
  * I:アイテム W：武器 A：防具
  * [id]：アイテムID
  * [rate]：確率
  * [condNameTag]：任意の条件タグ名（省略可）省略した場合はConditionalDropが指定されます。
+ * [condMode]：条件モード（省略可）0:一部一致 1:全て一致
  * 
- * <Drop[condNameTag]:[id],[id],[id]...> 攻撃者が指定したIDの条件を満たしたときにドロップします。
- * <TargetDrop[condNameTag]:[id],[id],[id]...> 倒した敵が指定したIDの条件を全て満たしたときにドロップします。
+ * <Drop[condNameTag]:[id],[id],[id]...> 攻撃して倒したアクターが指定したIDの条件を満たしたときにドロップします。
+ * <TargetDrop[condNameTag]:[id],[id],[id]...> 倒された敵が指定したIDの条件を全て満たしたときにドロップします。
  * <PartyDrop[condNameTag]:[id],[id],[id]...> パーティメンバーの指定したIDの条件を全て満たしたときにドロップします。
  * <TroopDrop[condNameTag]:[id],[id],[id]...> 敵グループの指定したIDの条件を全て満たしたときにドロップします。
  * [id]:条件付きベースの適用条件のリストID
- * <[condNameTag]Partial> 上記の条件判定をいずれかの条件が一致したときに条件を満たすようにします。未記入の場合は全ての条件を満たしたときになります。
  * 例
  * <CondDropItem:I,16,50,cond1> 条件cond1が一致したときにアイテム番号16番のアイテムが５０％の確率でドロップします。
  * <DropCond1:1>上記の条件を参照するためのタグでリスト番号１番の条件を判定します。
- * <Cond1Partial>上記の条件が全て一致したときに条件を満たすようにします。
  * 
  * このプラグインはNUUN_ConditionsBaseが必要です。
  * 
@@ -41,6 +40,9 @@
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2021/11/12 Ver.1.0.2
+ * 条件付きベースの定義変更による条件タグの設定方法を変更。
+ * ターゲットデータが取得できない問題を修正。
  * 2021/10/24 Ver.1.0.1
  * 条件タグにスペースを入れると条件が判定されない問題を修正。
  * 2021/10/22 Ver.1.0.0
@@ -54,16 +56,7 @@ Imported.NUUN_ConditionalDrops = true;
 
 (() => {
   const parameters = PluginManager.parameters('NUUN_ConditionalDrops');
-  const actionData = {};
   let conditionalDropItems = [];
-
-  const _Game_Action_executeDamage = Game_Action.prototype.executeDamage;
-  Game_Action.prototype.executeDamage = function(target, value) {
-    actionData.subject = this.subject();
-    actionData.action = this;
-    actionData.damage = value;
-    _Game_Action_executeDamage.call(this, target, value);
-  };
 
   const _Game_Enemy_setup = Game_Enemy.prototype.setup;
   Game_Enemy.prototype.setup = function(enemyId, x, y) {
@@ -79,9 +72,12 @@ Imported.NUUN_ConditionalDrops = true;
 
   Game_Enemy.prototype.getConditionalDrops = function() {
     this._getCondDropList = [];
+    const enemy = this.enemy()
     conditionalDropItems.forEach(condDrop => {
-      const condTag = String(condDrop[3]).trim() || 'CondDrop';
-      if (actionData.subject.getTriggerConditions(this.enemy(), this.enemy(), 'Target' + condTag, condTag, 'Party' + condTag, 'Troop' + condTag, actionData.action, actionData.damage, condTag + 'Partial') && this.condDropRate(condDrop)) {
+      const condTag = "Drop" + String(condDrop[3]).trim() || 'DropCond';
+      const action = $gameTemp.getActionData();
+      const mode = condDrop[4] || 0;
+      if (action.subject.getTriggerConditions(enemy, this, condTag, 'Target' + condTag, 'Party' + condTag, 'Troop' + condTag, action.action, action.damage, mode) && this.condDropRate(condDrop)) {
         const drop = this.getCondDropItem(condDrop);
         if (drop) {
           this._getCondDropList.push(drop);

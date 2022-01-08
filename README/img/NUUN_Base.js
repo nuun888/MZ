41d7d0fc4046f88b897962e395043833b4f56e4b
nuun888 <70774024+nuun888@users.noreply.github.com>
2022-01-08 19:07:48 +0900

    Add files via upload

---
+++ README/img/NUUN_Base.js
@@ -0,0 +1,209 @@
+﻿/*:-----------------------------------------------------------------------------------
+ * NUUN_Base.js
+ * 
+ * Copyright (C) 2020 NUUN
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ * -------------------------------------------------------------------------------------
+ * 
+ */ 
+/*:
+ * @target MZ
+ * @plugindesc  共通処理
+ * @author NUUN
+ * @version 1.4.0
+ * 
+ * @help
+ * 共通処理を行うベースプラグインです。
+ * プラグインリストの上に配置してください。
+ * 
+ * 利用規約
+ * このプラグインはMITライセンスで配布しています。
+ * 
+ * 更新履歴
+ * 2022/1/8 Ver.1.4.0
+ * カラーインデックス取得処理を追加。
+ * 2021/12/12 Ver.1.3.2
+ * 敵ステート表示拡張を適用するための処理を追加。
+ * 2021/11/7 Ver.1.3.1
+ * 複数属性取得に関しての処理追加。
+ * 2021/8/22 Ver.1.3.0
+ * Window_ItemListに処理を追加。
+ * 2021/7/15 Ver.1.2.0
+ * ゲージ、名前表示関連プラグインの共通処理を追加。
+ * 2021/5/15 Ver.1.1.4
+ * ウィンドウスキンをウィンドウ毎に設定できる処理を追加。
+ * 2021/5/8 Ver.1.1.3
+ * 処理を一部修正。
+ * 2021/5/7 Ver.1.1.2
+ * 構造体の取得処理を追加。
+ * 2021/4/23 Ver.1.1.1
+ * 画像のフォルダー指定の処理を追加。
+ * 2021/3/14 Ver.1.1.0
+ * 画像のフォルダー指定の処理を追加。
+ * 2020/12/31 Ver.1.0.0
+ * 初版
+ * 
+ */
+var Imported = Imported || {};
+Imported.NUUN_Base = true;
+const NUUN_Base_Ver = 140;
+
+function NuunManager() {
+  throw new Error("This is a static class");
+}
+
+(() => {
+const parameters = PluginManager.parameters('NUUN_Base');
+
+function structureData(params) {
+  return JSON.parse(JSON.stringify(params, function(key, value) {
+    try {
+        return JSON.parse(value);
+    } catch (e) {
+        try {
+            return eval(value);
+        } catch (e) {
+            return value;
+        }
+    }
+  }));
+}
+
+function nuun_GausePlugins() {
+  return Imported.NUUN_ButlerHPGauge || Imported.NUUN_ButlerName || Imported.NUUN_EnemyTpbGauge || Imported.NUUN_EnemyStateIconEX;
+}
+
+NuunManager.getColorCode = function(color) {
+  if (typeof(color) === "string") {
+    return color;
+  }
+  return ColorManager.textColor(color);
+};
+
+DataManager.nuun_structureData = function(params){
+  return params ? structureData(params) : [];
+};
+
+const _Scene_Boot_onDatabaseLoaded = Scene_Boot.prototype.onDatabaseLoaded;
+Scene_Boot.prototype.onDatabaseLoaded = function() {
+  _Scene_Boot_onDatabaseLoaded.call(this);
+  DataManager.nuun_Data();
+};
+
+DataManager.nuun_Data = function(){
+  DataManager.nuun_DataObject($dataActors, 1);
+  DataManager.nuun_DataObject($dataEnemies, 7);
+};
+
+DataManager.nuun_DataObject = function(object, code){
+  for(let i = 1; i <= object.length; i++){
+    if (object[i]) {
+      if(code === 1) {
+        DataManager.nuun_loadDataActors(object[i]);
+      } else if (code === 7) {
+        DataManager.nuun_loadDataEnemies(object[i]);
+      }
+    }
+  }
+};
+
+DataManager.nuun_loadDataActors = function(deta){
+};
+
+DataManager.nuun_loadDataEnemies = function(deta){
+};
+
+ImageManager.nuun_backGround = function(filename) {
+  return this.loadBitmap("img/nuun_background/", filename);
+};
+
+ImageManager.nuun_actorPictures = function(filename) {
+  return this.loadBitmap("img/nuun_actorpictures/", filename);
+};
+
+ImageManager.nuun_LoadPictures = function(filename) {
+  return this.loadBitmap("img/", filename);
+};
+
+const _BattleManager_initMembers = BattleManager.initMembers;
+BattleManager.initMembers = function() {
+  _BattleManager_initMembers.call(this);
+  this.gaugeBaseSprite = null;
+};
+
+const _Window_Selectable_drawItemBackground = Window_Selectable.prototype.drawItemBackground;
+Window_Selectable.prototype.drawItemBackground = function(index) {
+  if (!this._contentsBackVisible) {
+    _Window_Selectable_drawItemBackground.call(this, index);
+  }
+};
+
+const _Window_Base_initialize = Window_Base.prototype.initialize;
+Window_Base.prototype.initialize = function(rect) {
+  _Window_Base_initialize.call(this, rect);
+  this._userWindowSkin = null;
+};
+
+const _Window_Base_loadWindowskin = Window_Base.prototype.loadWindowskin;
+Window_Base.prototype.loadWindowskin = function() {
+  if (this._userWindowSkin) {
+    this.windowskin = ImageManager.loadSystem(this._userWindowSkin);
+  } else {
+    _Window_Base_loadWindowskin.call(this);
+  }
+};
+
+const _Spriteset_Battle_createLowerLayer = Spriteset_Battle.prototype.createLowerLayer;
+Spriteset_Battle.prototype.createLowerLayer = function() {
+  _Spriteset_Battle_createLowerLayer.call(this);
+  if (nuun_GausePlugins()) {
+    this.createGaugeBase();
+  }
+};
+
+Spriteset_Battle.prototype.createGaugeBase = function() {
+  if (!this._butlerGaugeBase) {
+    const sprite = new Sprite();
+    this.addChild(sprite);
+    this._butlerGaugeBase = sprite;
+    BattleManager.gaugeBaseSprite = sprite;
+  }
+};
+
+const _Spriteset_Battle_update = Spriteset_Battle.prototype.update;
+Spriteset_Battle.prototype.update = function() {
+  _Spriteset_Battle_update.call(this);
+  this.updateButlerGauge();
+};
+
+Spriteset_Battle.prototype.updateButlerGauge = function() {
+  if (this._butlerGaugeBase) {
+    for (const sprite of this._butlerGaugeBase.children) {
+      const spriteData = this._enemySprites.some(enemy => enemy.spriteId === sprite.enemySpriteId);
+      if (!spriteData) {
+        this._butlerGaugeBase.removeChild(sprite);
+      }
+    }
+  }
+};
+
+const _Window_ItemList_initialize  = Window_ItemList.prototype.initialize;
+Window_ItemList.prototype.initialize = function(rect) {
+  _Window_ItemList_initialize.call(this, rect);
+  this._className = String(this.constructor.name);
+};
+
+Window_ItemList.prototype.isConstructor = function() {
+  return false;
+};
+
+Game_Action.prototype.getAttackElementsList = function() {
+  return Imported.NUUN_MultiElement ? this.getAttackElements() : this.subject().attackElements();
+};
+
+Game_Action.prototype.getItemElementsList = function() {
+  return Imported.NUUN_MultiElement ? this.getItemElements() : [this.item().damage.elementId];
+};
+
+})();
---
+++ README/img/NUUN_DamagePopUpImg.js
@@ -0,0 +1,220 @@
+/*:-----------------------------------------------------------------------------------
+ * NUUN_DamagePopUpImg.js
+ * 
+ * Copyright (C) 2021 NUUN
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ * -------------------------------------------------------------------------------------
+ */ 
+/*:
+ * @target MZ
+ * @plugindesc ダメージポップアップ画像化
+ * @author NUUN
+ * @version 1.1.0
+ * @base NUUN_Base
+ * @orderAfter NUUN_Base
+ * @orderBefore NUUN_DamagePopUpSimulDisplay
+ *            
+ * @help
+ * ダメージポップアップをツクールMVと同じ仕様に変更します。（文字ではなく画像で表示）
+ * 素材規格は縦分割数で設定した数値で割り切れる高さにしてください。横幅は指定はありません。
+ * TP用のポップアップ（ダメージ用、回復用）を追加する場合は縦分割数を7にしてください。
+ * 画像のインデックス番号は一番上から0で始まります。
+ * なおツクールMVのダメージポップアップ画像「Damage.png」を使用するにはのツクールMVのユーザ登録が必要です。
+ * 
+ * 
+ * 利用規約
+ * このプラグインはMITライセンスで配布しています。
+ * 
+ * 更新履歴
+ * 2022/1/8 Ver 1.1.0
+ * 割り当てられる画像の種類を増やせるように修正。
+ * 各ポップアップ画像のインデックス番号を指定できるように修正。
+ * 2021/7/22 Ver 1.0.0
+ * 初版
+ * 
+ * @param PopUpDamageImg
+ * @desc ダメージ画像のファイル名を指定します。
+ * @text ダメージ画像
+ * @type file[]
+ * @dir img/
+ * @default 
+ * 
+ * @param BitmapBaseRows
+ * @desc 画像の縦の分割数
+ * @text 縦分割数
+ * @type number
+ * @default 5
+ * @min 1
+ * 
+ * @param HPDamageBaseRow
+ * @desc HPダメージのポップアップの画像インデックス
+ * @text HPダメージ画像インデックス
+ * @type number
+ * @default 0
+ * @min 0
+ * 
+ * @param HPRecoveryBaseRow
+ * @desc HP回復のポップアップの画像インデックス
+ * @text HP回復画像インデックス
+ * @type number
+ * @default 1
+ * @min 0
+ * 
+ * @param MPDamageBaseRow
+ * @desc MPダメージのポップアップの画像インデックス
+ * @text MPダメージ画像インデックス
+ * @type number
+ * @default 2
+ * @min 0
+ * 
+ * @param MPRecoveryBaseRow
+ * @desc MP回復のポップアップの画像インデックス
+ * @text MP回復画像インデックス
+ * @type number
+ * @default 3
+ * @min 0
+ * 
+ * @param TPDamageBaseRow
+ * @desc TPダメージのポップアップの画像インデックス
+ * @text TPダメージ画像インデックス
+ * @type number
+ * @default 0
+ * @min 0
+ * 
+ * @param TPRecoveryBaseRow
+ * @desc TP回復のポップアップの画像インデックス
+ * @text TP回復画像インデックス
+ * @type number
+ * @default 1
+ * @min 0
+ * 
+ * @param MissBaseRow
+ * @desc 失敗時のポップアップの画像インデックス
+ * @text 失敗時画像インデックス
+ * @type number
+ * @default 4
+ * @min 0
+ * 
+ */
+
+var Imported = Imported || {};
+Imported.NUUN_DamagePopUpImg = true;
+
+(() => {
+  const parameters = PluginManager.parameters('NUUN_DamagePopUpImg');
+  const PopUpDamageImg = (NUUN_Base_Ver >= 113 ? (DataManager.nuun_structureData(parameters['PopUpDamageImg'])) : null) || [];
+  const HPDamageBaseRow = Number(parameters['HPDamageBaseRow'] || 0);
+  const HPRecoveryBaseRow = Number(parameters['HPRecoveryBaseRow'] || 1);
+  const MPDamageBaseRow = Number(parameters['MPDamageBaseRow'] || 2);
+  const MPRecoveryBaseRow = Number(parameters['MPRecoveryBaseRow'] || 3);
+  const TPDamageBaseRow = Number(parameters['TPDamageBaseRow'] || 0);
+  const TPRecoveryBaseRow = Number(parameters['TPRecoveryBaseRow'] || 1);
+  const MissBaseRow = Number(parameters['MissBaseRow'] || 4);
+
+  const _Scene_Boot_loadSystemImages = Scene_Boot.prototype.loadSystemImages;
+  Scene_Boot.prototype.loadSystemImages = function() {
+    _Scene_Boot_loadSystemImages.call(this);
+    if (PopUpDamageImg && PopUpDamageImg[0]) {
+      ImageManager.nuun_LoadPictures(PopUpDamageImg[0]);
+    }
+  };
+
+
+  const _Sprite_Damage_initialize = Sprite_Damage.prototype.initialize;
+  Sprite_Damage.prototype.initialize = function() {
+    _Sprite_Damage_initialize.call(this);
+    if (this.constructor === Sprite_Damage) {
+      this._damageBitmap = (PopUpDamageImg && PopUpDamageImg[0]) ? ImageManager.nuun_LoadPictures(PopUpDamageImg[0]) : null;
+    }
+    this._baseRow = 0;
+    this._statusType = null;
+  };
+
+  Sprite_Damage.prototype.digitWidth = function() {
+    return this._damageBitmap ? this._damageBitmap.width / 10 : 0;
+  };
+
+  Sprite_Damage.prototype.digitHeight = function() {
+    return this._damageBitmap ? this._damageBitmap.height / 5 : 0;
+  };
+  
+  const _Sprite_Damage_createBitmap = Sprite_Damage.prototype.createBitmap;
+  Sprite_Damage.prototype.createBitmap = function(width, height) {
+    if (this._damageBitmap) {
+      return ImageManager.nuun_LoadPictures(PopUpDamageImg[0]);
+    } else {
+      return _Sprite_Damage_createBitmap.call(this, width, height);
+    }
+  };
+
+  Sprite_Damage.prototype.destroy = function(options) {//再定義
+    for (const child of this.children) {
+      if (child.bitmap && !child.bitmap._image) {
+          child.bitmap.destroy();
+      }
+    }
+    Sprite.prototype.destroy.call(this, options);
+  };
+
+  const _Sprite_Damage_setup = Sprite_Damage.prototype.setup;
+  Sprite_Damage.prototype.setup = function(target) {
+    const result = target.result();
+    this._statusType = null;
+    if (result.missed || result.evaded) {
+      this._statusType = 'miss';
+    } else if (result.hpAffected) {
+      this._statusType = 'hp';
+    } else if (result.mpDamage !== 0) {
+      this._statusType = 'mp';
+    } else if (result.tpDamage !== 0) {
+      this._statusType = 'tp';
+    }
+    _Sprite_Damage_setup.call(this, target);
+  };
+
+  const _Sprite_Damage_createMiss = Sprite_Damage.prototype.createMiss;
+  Sprite_Damage.prototype.createMiss = function() {
+    if (this._damageBitmap) {
+      const w = this.digitWidth();
+      const h = this.digitHeight();
+      const sprite = this.createChildSprite();
+      sprite.setFrame(0, MissBaseRow * h, 4 * w, h);
+      sprite.dy = 0;
+    } else {
+      _Sprite_Damage_createMiss.call(this);
+    }
+  };
+
+  const _Sprite_Damage_createDigits = Sprite_Damage.prototype.createDigits;
+  Sprite_Damage.prototype.createDigits = function(value) {
+    if (this._damageBitmap) {
+      const string = Math.abs(value).toString();
+      const row = this.getBaseRow(value) + (value < 0 ? 1 : 0);
+      const w = this.digitWidth();
+      const h = this.digitHeight();
+      for (var i = 0; i < string.length; i++) {
+        const sprite = this.createChildSprite();
+        const n = Number(string[i]);
+        sprite.setFrame(n * w, row * h, w, h);
+        sprite.x = (i - (string.length - 1) / 2) * w;
+        sprite.dy = -i;
+      }
+    } else {
+      _Sprite_Damage_createDigits.call(this, value);
+    }
+  };
+
+  Sprite_Damage.prototype.getBaseRow = function(value) {
+    if (this._statusType === 'hp') {
+      return value > 0 ? HPDamageBaseRow : HPRecoveryBaseRow;
+    } if (this._statusType === 'mp') {
+      return value > 0 ? MPDamageBaseRow : MPRecoveryBaseRow;
+    } else if (this._statusType === 'tp') {
+      return value > 0 ? TPDamageBaseRow : TPRecoveryBaseRow;
+    } else {
+      return value > 0 ? 0 : 1;
+    }
+  };
+
+})();

b4340695fc9eb7bdcc6fb348568fae2538bdba9a
nuun888 <70774024+nuun888@users.noreply.github.com>
2022-01-16 00:49:12 +0900

    Add files via upload

--- NUUN_AddStateDeviation.js
+++ NUUN_AddStateDeviation.js
@@ -6,13 +6,12 @@
  * http://opensource.org/licenses/mit-license.php
  * -------------------------------------------------------------------------------------
  * 
- * 更新履歴
- * 2020/12/17 Ver.1.0.0
  */ 
 /*:
  * @target MZ
  * @plugindesc ステート付与の仕様変更
  * @author NUUN
+ * @version 1.0.1
  * 
  * @help
  * 必中攻撃ではステートを付与する際に有効度、運を無視して付与されてしまいます。
@@ -27,17 +26,22 @@
  * スキルのメモ欄に<CertainStateSkill>を記入することで、このスキルで付与する
  * ステートは有効度、運を無視して付与されます。
  * スキルのメモ欄に<CertainState:[id]> を記入することで、このスキルに付与する
- * ステート[id]のステートは有効度、運を無視して付与します。[id]:ステート番号
+ * ステート[id]のステートは有効度、運を無視して付与します。[id]:ステートID
  * 
  * <CertainStateSkill>  このスキルで受けるステートは有効度、運を無視して付与します。
  * <CertainState:4,6> このスキルで受けるステート4,6は有効度、運を無視して付与します。
- *  
+ * 
  * 味方にステートを付与させる場合は必中にしても運を考慮してしまうためステートのメモ欄に<NoLukState>を記入することで運を無視して付与させることが出来ます。
  * ステート無効化を設定している場合、有効度を無視する攻撃をしてもステートは無効化されます。
  * 
- * 
  * 利用規約
  * このプラグインはMITライセンスで配布しています。
+ * 
+ * 更新履歴
+ * 2022/1/16 Ver.1.0.1
+ * 耐性無視時の攻撃時ステートの確率が適用されていなかった問題を修正。
+ * 2020/12/17 Ver.1.0.0
+ * 初版
  * 
  */
 var Imported = Imported || {};
@@ -63,28 +67,28 @@ Game_Action.prototype.noLukState = function(id){
 
 Game_Action.prototype.itemEffectAddAttackState = function(target, effect) {
   for (const stateId of this.subject().attackStates()) {
-      let chance = effect.value1;
-      if(!this.certainState(stateId)) {
-        chance *= target.stateRate(stateId);
-        chance *= this.subject().attackStatesRate(stateId);
-        chance *= this.noLukState(stateId) ? 1.0 : this.lukEffectRate(target);
-      }
-      if (Math.random() < chance) {
-          target.addState(stateId);
-          this.makeSuccess(target);
-      }
+    let chance = effect.value1;
+    chance *= this.subject().attackStatesRate(stateId);
+    if(!this.certainState(stateId)) {
+      chance *= target.stateRate(stateId);
+      chance *= this.noLukState(stateId) ? 1.0 : this.lukEffectRate(target);
+    }
+    if (Math.random() < chance) {
+      target.addState(stateId);
+      this.makeSuccess(target);
+    }
   }
 };
 
 Game_Action.prototype.itemEffectAddNormalState = function(target, effect) {
   let chance = effect.value1;
-  if(!this.certainState(effect.dataId)){
+  if (!this.certainState(effect.dataId)) {
     chance *= target.stateRate(effect.dataId);
     chance *= this.noLukState(effect.dataId) ? 1.0 : this.lukEffectRate(target);
   }
   if (Math.random() < chance) {
-    target.addState(effect.dataId);
-    this.makeSuccess(target);
+      target.addState(effect.dataId);
+      this.makeSuccess(target);
   }
 };
 })();

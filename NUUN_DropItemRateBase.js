cb4d97acea71a72dfeefc969d03efcebc11586e2
nuun888 <70774024+nuun888@users.noreply.github.com>
2022-01-29 09:31:31 +0900

    Add files via upload

---
+++ NUUN_DropItemRateBase.js
@@ -0,0 +1,49 @@
+/*:-----------------------------------------------------------------------------------
+ * NUUN_DropItemRateBase.js
+ * 
+ * Copyright (C) 2022 NUUN
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ * -------------------------------------------------------------------------------------
+ */ 
+/*:
+ * @target MZ
+ * @plugindesc ドロップ率確率操作ベース
+ * @author NUUN
+ * @version 1.0.0
+ * 
+ * @help
+ * 敵のドロップ率の確率に変更を加えるプラグインを適用させるためのベースプラグインです。
+ * 
+ * 更新履歴
+ * 2022/1/29 Ver.1.0.0
+ * 初版
+ * 
+ * 
+ */
+var Imported = Imported || {};
+Imported.NUUN_DropItemRateBase = true;
+
+(() => {
+const parameters = PluginManager.parameters('NUUN_DropItemRateBase');
+
+Game_Enemy.prototype.makeDropItems = function() {//再定義
+    const rate = this.dropItemRate();
+    return this.enemy().dropItems.reduce((r, di) => {
+        if (di.kind > 0 && this.randomValue(di, rate) < this.rateValue(di, rate)) {
+            return r.concat(this.itemObject(di.kind, di.dataId));
+        } else {
+            return r;
+        }
+    }, []);
+};
+
+Game_Enemy.prototype.randomValue = function(di, rate) {
+    return Math.random() * di.denominator;
+};
+
+Game_Enemy.prototype.rateValue = function(di, rate) {
+    return rate;
+};
+
+})();

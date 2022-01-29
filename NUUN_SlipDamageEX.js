8a9afe2fcb055256894a4a263709f18d1060a238
nuun888 <70774024+nuun888@users.noreply.github.com>
2022-01-29 16:46:55 +0900

    Add files via upload

--- NUUN_SlipDamageEX.js
+++ NUUN_SlipDamageEX.js
@@ -10,7 +10,7 @@
  * @target MZ
  * @plugindesc スリップダメージ拡張
  * @author NUUN
- * @version 1.0.1
+ * @version 1.1.0
  * @base NUUN_StateTurnCount
  * @orderAfter NUUN_StateTurnCount
  * 
@@ -22,6 +22,9 @@
  * <SlipDamageHP:[eval]> HPのスリップダメージを設定します。
  * <SlipDamageMP:[eval]> MPのスリップダメージを設定します。
  * <SlipDamageTP:[eval]> TPのスリップダメージを設定します。
+ * <SlipDamageFixedHP:[eval]> HPのスリップダメージを固定値で設定します。
+ * <SlipDamageFixedMP:[eval]> MPのスリップダメージを固定値で設定します。
+ * <SlipDamageFixedTP:[eval]> TPのスリップダメージを固定値で設定します。
  * [eval]:評価式
  * b:バトラーゲームデータ
  * db:バトラーのデータベースデータ
@@ -30,11 +33,14 @@
  * <SlipDamageHP:-10 * st> 毎ターンごとに10%加算した割合のダメージを受けます。
  * <SlipDamageMP:10 * st> 毎ターンごとに10%加算した割合で回復します。
  * <SlipDamageHP:Math.pow(3, st) * -1> 毎ターンごとに3%ずつスリップダメージが倍化します。
+ * <SlipDamageFixedHP:-10> 毎ターンごとに１０のスリップダメージを受けます。
  * 
  * 利用規約
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2022/1/29 Ver.1.1.0
+ * スリップダメージに固定値を設定できる機能を追加。
  * 2022/1/16 Ver.1.0.1
  * 経過ターンの処理を別プラグイン化。
  * 2022/1/11 Ver.1.0.0
@@ -62,12 +68,27 @@ Game_Battler.prototype.slipDamageEX = function(type) {
     return slipDamage;
 };
 
+Game_Battler.prototype.slipDamageFixedEX = function(type) {
+    let slipDamage = 0;
+    const tag = 'SlipDamageFixed' + type;
+    const b = this;
+    const db = this.isActor() ? this.actor() : this.enemy();
+    for (const stateId of this._states) {
+        const data = $dataStates[stateId].meta[tag];
+        if (data) {
+            const st = this.isStateNowTurn(stateId);
+            slipDamage += eval(data);
+        }
+    }
+    return slipDamage;
+};
+
 const _Game_Battler_regenerateHp = Game_Battler.prototype.regenerateHp;
 Game_Battler.prototype.regenerateHp = function() {
     _Game_Battler_regenerateHp.call(this);
     const minRecover = -this.maxSlipDamage();
     const hpDamage = this._result.hpDamage;
-    const value = Math.max(Math.floor(this.mhp * this.slipDamageEX('HP')), minRecover);
+    const value = Math.max(Math.floor(this.mhp * this.slipDamageEX('HP')) + this.slipDamageFixedEX('HP'), minRecover);
     if (value !== 0) {
         this.gainHp(value);
         this._result.hpDamage += hpDamage;
@@ -78,7 +99,7 @@ const _Game_Battler_regenerateMp = Game_Battler.prototype.regenerateMp;
 Game_Battler.prototype.regenerateMp = function() {
     _Game_Battler_regenerateMp.call(this);
     const mpDamage = this._result.mpDamage;
-    const value = Math.floor(this.mmp * this.slipDamageEX('MP'));
+    const value = Math.floor(this.mmp * this.slipDamageEX('MP') + this.slipDamageFixedEX('MP'));
     if (value !== 0) {
         this.gainMp(value);
         this._result.mpDamage += mpDamage;
@@ -89,7 +110,7 @@ const _Game_Battler_regenerateTp = Game_Battler.prototype.regenerateTp;
 Game_Battler.prototype.regenerateTp = function() {
     _Game_Battler_regenerateTp.call(this);
     const tpDamage = this._result.tpDamage;
-    const value = Math.floor(100 * this.slipDamageEX('TP'));
+    const value = Math.floor(100 * this.slipDamageEX('TP') + this.slipDamageFixedEX('TP'));
     if (value !== 0) {
         this.gainSilentTp(value);
         this._result.tpDamage += tpDamage;

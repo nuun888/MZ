fbad1026b190825289a9a250386c09fa7adcf1a9
nuun888 <70774024+nuun888@users.noreply.github.com>
2022-07-31 23:03:58 +0900

    Add files via upload

--- NUUN_MaxBattleMembers.js
+++ NUUN_MaxBattleMembers.js
@@ -10,8 +10,9 @@
  * @target MZ
  * @plugindesc 戦闘メンバー数変更プラグイン
  * @author NUUN
- * @version 1.0.3
- * 
+ * @version 1.0.4
+ * @base NUUN_Base
+ * @orderAfter NUUN_Base
  * 
  * @help
  * 戦闘参加メンバーの人数を変更します。またゲーム途中で最大バトルメンバー数を変更できます。
@@ -20,6 +21,8 @@
  * 最大戦闘メンバー数を前の数値より高く変更した場合、セーブ後のデータでは前の最大戦闘メンバー数よりフォロワーの画像が表示されません。
  * 
  * 更新履歴
+ * 2022/7/31 Ver.1.0.4
+ * 戦闘メンバーが５人以上の時にゲージがはみ出さないように修正。
  * 2022/2/27 Ver.1.0.3
  * 戦闘中に最大メンバーの増減をさせるとエラーがでる問題を修正。
  * 2022/2/11 Ver.1.0.2
@@ -85,6 +88,13 @@ Game_Party.prototype.setMaxBattleMembers = function(num) {
   $gamePlayer.refresh();
 };
 
+
+const _Window_BattleStatus_initialize = Window_BattleStatus.prototype.initialize;
+Window_BattleStatus.prototype.initialize = function(rect) {
+  _Window_BattleStatus_initialize.call(this, rect);
+  BattleManager.rectMaxWidth = this.itemRectWithPadding(0).width;
+};
+
 Window_BattleStatus.prototype.maxCols = function() {//再定義
   return $gameParty._maxBattleMembers;
 };
@@ -101,6 +111,64 @@ Scene_Battle.prototype.update = function() {
       this.commandCancel();
     }
   }
+};
+
+
+Sprite.prototype.gaugeFixWidthClass = function() {
+  switch (this.className) {
+    case 'Window_BattleStatus':
+    case 'Window_BattleActor':
+      return true;
+  }
+  return false;
+};
+
+
+const _Sprite_Gauge_initMembers = Sprite_Gauge.prototype.initMembers;
+Sprite_Gauge.prototype.initMembers = function() {
+  _Sprite_Gauge_initMembers.call(this);
+  this._gaugeWidth = null;
+};
+
+const _Sprite_Gauge_setup = Sprite_Gauge.prototype.setup;
+Sprite_Gauge.prototype.setup = function(battler, statusType) {
+  _Sprite_Gauge_setup.call(this, battler, statusType);
+  if (this.gaugeFixWidthClass()) {
+    const width = BattleManager.rectMaxWidth;
+    if (this.bitmapWidth() !== width) {
+      this._gaugeWidth = width;
+      this.redraw();
+    }
+  }
+};
+
+const _Sprite_Gauge_bitmapWidth = Sprite_Gauge.prototype.bitmapWidth
+Sprite_Gauge.prototype.bitmapWidth = function() {
+  return this._gaugeWidth ? this._gaugeWidth : _Sprite_Gauge_bitmapWidth.call(this);
+};
+
+
+const _Sprite_Name_initMembers = Sprite_Name.prototype.initMembers;
+Sprite_Name.prototype.initMembers = function() {
+  _Sprite_Name_initMembers.call(this);
+  this._nameWidth = null;
+};
+
+const _Sprite_Name_setup = Sprite_Name.prototype.setup;
+Sprite_Name.prototype.setup = function(battler) {
+  if (this.gaugeFixWidthClass()) {
+    const width = BattleManager.rectMaxWidth;
+    if (this.bitmapWidth() !== width) {
+      this._nameWidth = width;
+      this.redraw();
+    }
+  }
+  _Sprite_Name_setup.call(this, battler);
+};
+
+const _Sprite_NamebitmapWidth = Sprite_Name.prototype.bitmapWidth;
+Sprite_Name.prototype.bitmapWidth = function() {
+  return this._nameWidth ? this._nameWidth : _Sprite_NamebitmapWidth.call(this);
 };
 
 })();

15831260ed8c1b3399927b0ab42073460f31b76f
nuun888 <70774024+nuun888@users.noreply.github.com>
2022-11-05 12:28:35 +0900

    Add files via upload

--- NUUN_AnimationFPSRate.js
+++ NUUN_AnimationFPSRate.js
@@ -8,18 +8,22 @@
  */ 
 /*:
  * @target MZ
- * @plugindesc MVアニメーション再生時のフレームレート個別設定
+ * @plugindesc MV互換アニメーション再生時のフレームレート個別設定
  * @author NUUN
- * @version 1.0.2
+ * @version 1.1.0
  * 
  * @help
  * MVのアニメーションを再生するときのフレームレートをアニメーションごとに設定します。
- * ※4フレームで15FPS、2フレームで30FPS、1フレームで60FPS
+ * 
+ * FPSモード：15FPSで4フレーム、30FPSで2フレーム、60FPSで1フレーム
+ * フレームモード：フレームで15FPS、2フレームで30FPS、1フレームで60FPS
  * 
  * 利用規約
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2021/11/5 Ver.1.1.0
+ * フレーム値をFPS指定に変更。
  * 2021/12/9 Ver.1.0.2
  * NUUN_Baseを導入しなくても動作するように変更。
  * 2021/11/28 Ver.1.0.1
@@ -28,17 +32,25 @@
  * 初版
  * 
  * @param DefaultAnimationRate
- * @text 標準アニメーション再生フレーム数
- * @desc 標準の１アニメーションの再生フレーム数を指定します。(4標準)
- * @type number
- * @default 4
- * @min 1
+ * @text 標準アニメーション再生FPS数
+ * @desc 標準の１アニメーションの再生FPS数を指定します。(15標準)
+ * @type combo
+ * @option 15
+ * @option 30
+ * @option 60
+ * @default 15
  * 
  * @param AnimationSetting
  * @text アニメーションFPS設定
  * @desc アニメーションのFPSの設定を行います。
  * @default []
  * @type struct<FPSSettingList>[]
+ * 
+ * @param AnimationRateMode
+ * @text フレーム設定モード
+ * @desc アニメーションの指定方法をFPS指定にします。OFFでフレーム指定 Ver.1.0.2以前からアップデートした場合はOFFにしてください。
+ * @type boolean
+ * @default true
  * 
  */
 /*~struct~FPSSettingList:
@@ -51,10 +63,13 @@
  * 
  * @param AnimationRate
  * @text アニメーション再生フレーム数
- * @desc １アニメーションの再生フレーム数を指定します。(4標準)
+ * @desc １アニメーションの再生フレーム数を指定します。(15標準)
  * @type number
- * @default 4
- * @min 1
+ * @type combo
+ * @option 15
+ * @option 30
+ * @option 60
+ * @default 15
  * 
  */
 var Imported = Imported || {};
@@ -73,8 +88,6 @@ const param = JSON.parse(JSON.stringify(parameters, function(key, value) {
         }
     }
 }));
-//const DefaultAnimationRate = Number(parameters['DefaultAnimationRate'] || 4);
-//const AnimationSetting = (NUUN_Base_Ver >= 113 ? (DataManager.nuun_structureData(parameters['AnimationSetting'])) : null) || [];
 
 Sprite_AnimationMV.prototype.setupRate = function() {
     this._rate = this.getAnimationRate();
@@ -83,6 +96,11 @@ Sprite_AnimationMV.prototype.setupRate = function() {
 Sprite_AnimationMV.prototype.getAnimationRate = function() {
     const id = this._animation.id;
     const find = param.AnimationSetting.find(data => data.AnimationID === id);
-    return find ? find.AnimationRate : param.DefaultAnimationRate;
+    if (param.AnimationRateMode) {
+        return Math.min(60 / (find ? Number(find.AnimationRate) : Number(param.DefaultAnimationRate)), 1);
+    } else {
+        return find ? Number(find.AnimationRate) : Number(param.DefaultAnimationRate);
+    }
 };
+
 })();

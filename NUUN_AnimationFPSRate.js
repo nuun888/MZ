662ffc7b34f2dff7f97cef5dee558372dc4663f5
nuun888 <70774024+nuun888@users.noreply.github.com>
2022-11-09 23:46:53 +0900

    Add files via upload

--- NUUN_AnimationFPSRate.js
+++ NUUN_AnimationFPSRate.js
@@ -8,6 +8,75 @@
  */ 
 /*:
  * @target MZ
+ * @plugindesc Frame rate change when playing MV compatible animation
+ * @author NUUN
+ * @version 1.1.1
+ * 
+ * @help
+ * Set the frame rate for each animation when playing MV animation.
+ * 
+ * FPS Mode: 4 frames at 15FPS, 2 frames at 30FPS, 1 frame at 60FPS
+ * Frame mode: 15FPS for frame, 30FPS for 2 frames, 60FPS for 1 frame
+ * 
+ * Terms of Use
+ * This plugin is distributed under the MIT license.
+ * This plugin can be used for free or for a fee.
+ * 
+ * Log
+ * 11/9/2022 Ver.1.1.1
+ * Changed the display in languages other than Japanese to English.
+ * 11/5/2022 Ver.1.1.0
+ * Change the frame value to FPS designation.
+ * 12/9/2021 Ver.1.0.2
+ * Changed to work without introducing 'NUUN_Base'.
+ * 11/28/2021 Ver.1.0.1
+ * Added the ability to set the default playback speed.
+ * 11/28/2021 Ver.1.0.0
+ * first edition.
+ * 
+ * @param DefaultAnimationRate
+ * @text Standard animation playback FPS
+ * @desc Specifies the number of playback FPS for one standard animation. (15 default)
+ * @type combo
+ * @option 15
+ * @option 30
+ * @option 60
+ * @default 15
+ * 
+ * @param AnimationSetting
+ * @text Animation FPS setting
+ * @desc Set animation FPS.
+ * @default []
+ * @type struct<FPSSettingList>[]
+ * 
+ * @param AnimationRateMode
+ * @text Frame setting mode
+ * @desc Set the animation specification method to FPS. Specify frame with OFF If you have updated from Ver.1.0.2 or earlier, please turn it OFF.
+ * @type boolean
+ * @default true
+ * 
+ */
+/*~struct~FPSSettingList:
+ * 
+ * @param AnimationID
+ * @text Animation ID
+ * @desc Specifies the ID of the animation.
+ * @type animation
+ * @default 0
+ * 
+ * @param AnimationRate
+ * @text Animation playback frame count
+ * @desc Specifies the number of playback frames for one animation. (15 default)
+ * @type number
+ * @type combo
+ * @option 15
+ * @option 30
+ * @option 60
+ * @default 15
+ * 
+ */
+/*:ja
+ * @target MZ
  * @plugindesc MV互換アニメーション再生時のフレームレート変更
  * @author NUUN
  * @version 1.1.0
@@ -22,6 +91,8 @@
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2022/11/9 Ver.1.1.1
+ * 日本語以外での表示を英語表示に変更。
  * 2021/11/5 Ver.1.1.0
  * フレーム値をFPS指定に変更。
  * 2021/12/9 Ver.1.0.2
@@ -53,7 +124,7 @@
  * @default true
  * 
  */
-/*~struct~FPSSettingList:
+/*~struct~FPSSettingList:ja
  * 
  * @param AnimationID
  * @text アニメーションID

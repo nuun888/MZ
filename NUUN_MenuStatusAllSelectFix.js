a0fb46c7ec4b0de99a0b96dfa545a061c5a88994
nuun888 <70774024+nuun888@users.noreply.github.com>
2022-11-23 05:58:58 +0900

    Add files via upload

--- NUUN_MenuStatusAllSelectFix.js
+++ NUUN_MenuStatusAllSelectFix.js
@@ -10,7 +10,7 @@
  * @target MZ
  * @plugindesc メニュー画面の全体対象時のカーソル表示修正
  * @author NUUN
- * @version 1.0.0
+ * @version 1.0.1
  * 
  * @help
  * メニュー画面プラグインのアクターの表示列を２列にして味方全体対象アイテム、スキルを使用すると、
@@ -20,6 +20,8 @@
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2022/11/23 Ver.1.0.1
+ * 全体選択時でアクター表示行が2列以上の時に表示が乱れる問題を修正。
  * 2022/5/29 Ver.1.0.0
  * 初版
  * 
@@ -33,9 +35,9 @@ Imported.NUUN_MenuStatusAllSelectFix = true;
 Window_Selectable.prototype.refreshCursorForAll = function() {//再定義
     const maxItems = this.maxItems();
     if (maxItems > 0) {
-        const items = maxItems + (maxItems >= this.maxCols() && maxItems % this.maxCols() ? 0 : -1);
+        const items = maxItems > this.maxCols() ? (maxItems % this.maxCols() + 1) * this.maxCols() : maxItems;
         const rect = this.itemRect(0);
-        rect.enlarge(this.itemRect(items));
+        rect.enlarge(this.itemRect(items - 1));
         this.setCursorRect(rect.x, rect.y, rect.width, rect.height);
     } else {
         this.setCursorRect(0, 0, 0, 0);

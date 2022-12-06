63b05f098ec7f05eec6ff7cd987825675e2b1996
nuun888 <70774024+nuun888@users.noreply.github.com>
2022-12-06 19:43:14 +0900

    Add files via upload

--- NUUN_CastTimeCharge.js
+++ NUUN_CastTimeCharge.js
@@ -8,14 +8,39 @@
  */ 
 /*:
  * @target MZ
+ * @plugindesc  Cast time charge rate characteristics
+ * @author NUUN
+ * @version 1.0.1
+ * 
+ * @help
+ * You can add a feature that changes the charge rate during casting time.
+ * 
+ * Note fields with features (Actor, Class, Weapon, Armor, State, Enemy)
+ * <CastTimeCharge:[rate]> Cast time charge rate changes by [rate]%.
+ * <CastTimeCharge:50> Charge rate of cast time becomes 50%. (The speed at which it accumulates is halved)
+ * <CastTimeCharge:200> Cast time charge rate is doubled.
+ * <CastTimeCharge:0> Immediately activate the skill.
+ * 
+ * Terms of Use
+ * This plugin is distributed under the MIT license.
+ * 
+ * Log
+ * 12/6/2022 Ver.1.0.1
+ * Changed the display in languages other than Japanese to English.
+ * 3/11/2021 Ver.1.0.0
+ * First edition.
+ * 
+ */
+/*:ja
+ * @target MZ
  * @plugindesc  キャストタイムチャージ率特徴
  * @author NUUN
- * @version 1.0.0
+ * @version 1.0.1
  * 
  * @help
  * キャストタイム中のチャージ率を変化させる特徴を付けることが出来ます。
  * 
- * 特徴を有するメモ欄（アクター、職業、武器、防具、ステート、エネミー）
+ * 特徴を有するメモ欄（アクター、職業、武器、防具、ステート、敵キャラ）
  * <CastTimeCharge:[rate]> キャストタイムのチャージ率が[rate]%変化します。
  * <CastTimeCharge:50> キャストタイムのチャージ率が５０％になります。（溜まるスピードが半分になります）
  * <CastTimeCharge:200> キャストタイムのチャージ率が２倍になります。
@@ -25,6 +50,8 @@
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2022/12/6 Ver.1.0.1
+ * 日本語以外での表示を英語表示に変更。
  * 2021/3/11 Ver.1.0.0
  * 初版
  * 

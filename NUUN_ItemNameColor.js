bfeefce570bec1cd64dbc1aa5a572b24a2d1c01e
nuun888 <70774024+nuun888@users.noreply.github.com>
2022-12-17 20:13:33 +0900

    Add files via upload

--- NUUN_ItemNameColor.js
+++ NUUN_ItemNameColor.js
@@ -8,13 +8,48 @@
  */ 
 /*:
  * @target MZ
+ * @plugindesc Item/skill name color
+ * @author NUUN
+ * @version 1.1.2
+ * 
+ * @help
+ * You can specify the color for item and skill text.
+ * 
+ * Item/Skill notes
+ * <NameColor:[id]> Change the text color of item and skill names. [id]: color index
+ * Example:<NameColor:17> The text color of items and skills will be the 17th color of the color index.
+ * <NameColor:#RRGGBB> Specify a color code.
+ * Example:<NameColor:#00bfff>
+ * 
+ * 
+ * Terms of Use
+ * This plugin is distributed under the MIT license.
+ * 
+ * Log
+ * 12/17/2022 Ver.1.1.2
+ * Changed the display in languages other than Japanese to English.
+ * Processing fixes.
+ * 5/10/2021 Ver.1.1.1
+ * Fixed the problem that an error occurs when selecting equipment on the equipment screen when using ”EquipScene_Extension” together.
+ * 3/10/2021 Ver.1.1.0
+ * Supports color code.
+ * 3/6/2021 Ver.1.0.2
+ * Changed not to process if the text color has already been changed by another plug-in.
+ * 2/7/2021 Ver.1.0.1
+ * Changed the timing of processing to return text color.
+ * 2/6/2021 Ver.1.0.0
+ * First edition.
+ */
+/*:
+ * @target MZ
  * @plugindesc  アイテム、スキルネームカラー
  * @author NUUN
- * @version 1.1.1
+ * @version 1.1.2
  * 
  * @help
  * アイテム、スキル欄の文字に色を指定できます。
  * 
+ * アイテム、スキルのメモ欄
  * <NameColor:[id]> アイテム、スキル名の文字色を変更します。[id]:カラーインデックス
  * 例：<NameColor:17> アイテム、スキルの文字色がカラーインデックス17番の色になります。
  * <NameColor:#RRGGBB> カラーコードを指定します。
@@ -25,6 +60,9 @@
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2022/12/17 Ver.1.1.2
+ * 日本語以外での表示を英語表示に変更。
+ * 処理の修正。
  * 2021/5/10 Ver.1.1.1
  * EquipScene_Extension併用時装備画面で装備を選択するとエラーが出る問題を修正。
  * 2021/3/10 Ver.1.1.0
@@ -71,8 +109,8 @@ Imported.NUUN_ItemNameColor = true;
   };
 
   Window_Base.prototype.changeNameColor = function(item) {
-    const color = item.meta.NameColor;
-    if (color) {
+    if (item.meta.NameColor) {
+      const color = item.meta.NameColor;
       if (typeof(color) === "string" && color.match(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/) !== null) {
         return color;
       }

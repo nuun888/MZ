1aa3d7e9bc7cd1f0dd879e7438946c9783d1d631
nuun888 <70774024+nuun888@users.noreply.github.com>
2023-01-03 21:28:50 +0900

    Add files via upload

--- NUUN_StateTurn.js
+++ NUUN_StateTurn.js
@@ -5,12 +5,119 @@
  * This software is released under the MIT License.
  * http://opensource.org/licenses/mit-license.php
  * -------------------------------------------------------------------------------------
- */ 
+ */
 /*:
+ * @target MZ
+ * @plugindesc State, buff remaining turn display
+ * @author NUUN
+ * @base NUUN_Base
+ * @orderAfter NUUN_Base
+ * @version 1.1.2
+ * 
+ * @help
+ * Show remaining turns on the state icon.
+ * 
+ * Display turn mode
+ * 'remaining':The default correction value when specified is 1.。
+ * 'elapsed':When specifying, set the number of turns correction to -1.
+ * "NUUN_StateTurnCount" is required to display elapsed turns.
+ * 
+ * Turn text color
+ * state notes
+ * <BatState> A state with this tag is a disadvantageous state. Therefore, the color of the disadvantageous state and debuff turn is applied.
+ * For states without the above tags, advantageous states and buff turn colors are applied.
+ * 
+ * Terms of Use
+ * This plugin is distributed under the MIT license.
+ * 
+ * Log
+ * 1/3/2023 Ver.1.1.2
+ * Changed the Type of color specification plug-in parameter to color. (Ver.1.6.0 or later)
+ * Changed the display in languages other than Japanese to English.
+ * 10/29/2022 Ver.1.1.1
+ * Added a function that allows you to change the character color of disadvantage, state, and buff turn numbers.
+ * 1/21/2022 Ver.1.1.0
+ * Added Elapsed Turns to how state turns are displayed. (Requires "NUUN_StateTurnCount")
+ * 9/16/2021 Ver.1.0.2
+ * Changed some function names to avoid conflicts.
+ * Removed unnecessary processing.
+ * 9/15/2021 Ver.1.0.1
+ * Fixed an issue where the turn display could not be acquired properly.
+ * Fixed an issue where turns in states without auto-release were displayed.
+ * 9/9/2021 Ver.1.0.0
+ * First edition.
+ * 
+ * @param TurnMode
+ * @desc Specifies the turn mode to display.
+ * @text Display turn mode
+ * @type select
+ * @option Remaining turn
+ * @value 'remaining'
+ * @option Elapsed turns (requires ``NUUN_StateTurnCount'')
+ * @value 'elapsed'
+ * @default 'remaining'
+ * 
+ * @param ActorStateIconVisible
+ * @desc Remaining turn indication on allied state icons.
+ * @text Allied state remaining turn display
+ * @type boolean
+ * @default true
+ * 
+ * @param EnemyStateIconVisible
+ * @desc Remaining turn display on enemy state icon.
+ * @text Enemy state remaining turn display
+ * @type boolean
+ * @default true
+ * 
+ * @param TurnX
+ * @desc Turn coordinate X. (relative)
+ * @text Turn coordinate X (relative)
+ * @type number
+ * @default 0
+ * @min -9999
+ * 
+ * @param TurnY
+ * @desc Turn coordinate Y. (relative)
+ * @text Turn coordinate Y (relative)
+ * @type number
+ * @default -4
+ * @min -9999
+ * 
+ * @param TurnFontSize
+ * @desc Font size for turns. (from main font)
+ * @text Turn font size
+ * @type number
+ * @default -4
+ * @min -9999
+ * 
+ * @param TurnCorrection
+ * @text Correction of the number of turns
+ * @desc Correct the display of the number of turns.
+ * @default 1
+ * @type number
+ * @min -9999
+ * @max 9999
+ * 
+ * @param TurnColor
+ * @text Advantageous state, color of buff turn
+ * @desc Advantageous state, system color number for debuff turns. (Color code can be entered from the text tab)
+ * @type color
+ * @default 0
+ * 
+ * @param BadTurnColor
+ * @text Unfavorable state, debuff turn color
+ * @desc System color number for unfavorable states and debuff turns. (Color code can be entered from the text tab)
+ * @type color
+ * @default 0
+ * 
+ */
+/*:ja
  * @target MZ
  * @plugindesc ステート、バフ残りターン表示
  * @author NUUN
- * @version 1.1.1
+ * @base NUUN_Base
+ * @orderAfter NUUN_Base
+ * @version 1.1.2
  * 
  * @help
  * ステートアイコンに残りターンを表示します。
@@ -22,13 +129,16 @@
  * 
  * ターンの文字色
  * ステートのメモ欄  
- * <BatState>　子のタグがあるステートは不利なステートになります。よって不利ステート、デバフターンの色が適用されます。  
+ * <BatState>　このタグがあるステートは不利なステートになります。よって不利ステート、デバフターンの色が適用されます。  
  * 上記タグがないステートは有利ステート、バフターンの色が適用されます。  
  * 
  * 利用規約
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2023/1/3 Ver.1.1.2
+ * カラー指定のプラグインパラメータのTypeをcolorに変更。(Ver.1.6.0以降)
+ * 日本語以外での表示を英語表示に変更。
  * 2022/10/29 Ver.1.1.1
  * 不利、ステート、バフのターン数の文字色を変更できる機能を追加。
  * 2022/1/21 Ver.1.1.0
@@ -96,13 +206,13 @@
  * @param TurnColor
  * @text 有利ステート、バフターンの色
  * @desc 有利なステート、デバフのターンのシステムカラー番号。(テキストタブからカラーコード入力可能)
- * @type number
+ * @type color
  * @default 0
  * 
  * @param BadTurnColor
  * @text 不利ステート、デバフターンの色
  * @desc 不利なステート、デバフのターンのシステムカラー番号。(テキストタブからカラーコード入力可能)
- * @type number
+ * @type color
  * @default 0
  * 
  */

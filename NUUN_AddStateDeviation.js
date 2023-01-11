e89dc0d935b1ea1950f70491d55b72a279d74637
nuun888 <70774024+nuun888@users.noreply.github.com>
2023-01-11 22:07:15 +0900

    Add files via upload

--- NUUN_AddStateDeviation.js
+++ NUUN_AddStateDeviation.js
@@ -11,23 +11,23 @@
  * @target MZ
  * @plugindesc Specification change of state assignment
  * @author NUUN
- * @version 1.0.2
+ * @version 1.0.3
  * 
  * @help
  * In the sure hit attack, when giving a state, it will be given without ignoring effectiveness and luck.
  * This plug-in will be changed to consider the effectiveness and luck even for hit hits.
- * Also, by writing <NoLukStateSkill> in the skill/item Notes, this skill will not be affected by luck when granting states.
- * By writing <NoLukState> in the memo field of the state, this state will not be affected by luck.
  * 
+ * Skill and Item notes
  * <NoLukStateSkill> The state granted by this skill ignores luck.
+ * 
+ * State notes
  * <NoLukState> This state is not affected by luck.
  * 
- * By entering <CertainStateSkill> in the memo field of the skill, the state granted by this skill will be granted regardless of effectiveness and luck.
- * By entering <CertainState:[id]> in the memo field of the skill, the state of state [id] to be given to this skill will be given regardless of effectiveness and luck.
- * [id]: State ID
- * 
- * <CertainStateSkill>  The state received by this skill ignores the effectiveness and luck.
- * <CertainState:4,6> State 4 and 6 received by this skill are granted regardless of effectiveness and luck.
+ * Skill notes
+ * <CertainStateSkill> The state received by this skill ignores the effectiveness and luck.
+ * <CertainState:[id],[id]....> The state of state [id] will be given regardless of effectiveness and luck.
+ * [id]:State id
+ * <CertainState:4,6> State 4 and 6 received by this skill are given regardless of effectiveness and luck.
  * 
  * When giving a state to an ally, luck is taken into account even if it is absolutely necessary, so you can ignore luck by entering <NoLukState> in the Note field of the state.
  * If the state invalidation is set, the state will be invalidated even if the attack ignores the effectiveness.
@@ -36,6 +36,8 @@
  * This plugin is distributed under the MIT license.
  * 
  * Log
+ * 1/11/2023 Ver.1.0.3
+ * Help fix.
  * 11/25/2022 Ver.1.0.2
  * Changed the display in languages other than Japanese to English.
  * 1/16/2022 Ver.1.0.1
@@ -48,33 +50,34 @@
  * @target MZ
  * @plugindesc ステート付与の仕様変更
  * @author NUUN
- * @version 1.0.2
+ * @version 1.0.3
  * 
  * @help
  * 必中攻撃ではステートを付与する際に有効度、運を無視して付与されてしまいます。
  * このプラグインでは必中攻撃でも有効度、運を考慮するように変更します。
- * また、スキル、アイテムのメモ欄に<NoLukStateSkill>を記入することで
- * このスキルは、ステートを付与する時に運の影響をうけません。
- * ステートのメモ欄に<NoLukState>を記入する事で、このステートは運の影響を受けません。
  * 
- * <NoLukStateSkill>  このスキルで付与するステートは運を影響を無視します。
- * <NoLukState> このステートは運の影響をうけません。
+ * スキル、アイテムのメモ欄
+ * <NoLukStateSkill> このスキルで付与するステートは運を影響を無視します。
  * 
- * スキルのメモ欄に<CertainStateSkill>を記入することで、このスキルで付与する
- * ステートは有効度、運を無視して付与されます。
- * スキルのメモ欄に<CertainState:[id]> を記入することで、このスキルに付与する
- * ステート[id]のステートは有効度、運を無視して付与します。[id]:ステートID
+ * ステートのメモ欄
+ * <NoLukState> このステートは運の影響を受けません。
  * 
- * <CertainStateSkill>  このスキルで受けるステートは有効度、運を無視して付与します。
+ * スキルのメモ欄
+ * <CertainStateSkill> このスキルで受けるステートは有効度、運を無視して付与します。
+ * <CertainState:[id],[id]....> ステート[id]のステートは有効度、運を無視して付与します。
+ * [id]:ステートID
  * <CertainState:4,6> このスキルで受けるステート4,6は有効度、運を無視して付与します。
  * 
  * 味方にステートを付与させる場合は必中にしても運を考慮してしまうためステートのメモ欄に<NoLukState>を記入することで運を無視して付与させることが出来ます。
  * ステート無効化を設定している場合、有効度を無視する攻撃をしてもステートは無効化されます。
  * 
+ * 
  * 利用規約
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2023/1/11 Ver.1.0.3
+ * ヘルプ修正。
  * 2022/11/25 Ver.1.0.2
  * 日本語以外での表示を英語表示に変更。
  * 2022/1/16 Ver.1.0.1

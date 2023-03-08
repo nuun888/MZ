532904cbd444374411a0f84132eb6e1478d1d769
nuun888 <70774024+nuun888@users.noreply.github.com>
2023-03-08 21:32:46 +0900

    Add files via upload

--- NUUN_BuffMaxLevel.js
+++ NUUN_BuffMaxLevel.js
@@ -10,7 +10,7 @@
  * @target MZ
  * @plugindesc バフ、デバフ重ね掛け上限変更
  * @author NUUN
- * @version 1.0.0
+ * @version 1.0.1
  * 
  * @help
  * バフ、デバフの重ね掛けの上限値を変更します。
@@ -19,9 +19,12 @@
  * アイコンの設定
  * バフ、デバフのアイコンインデックスは１段階につき指定した開始インデックス + バフ（デバフ）レベル * 8になっています。
  * デフォルトだとバフの開始インデックスが32、強化バフが40、デバフの開始インデックスが48、強化デバフが56になっています。
- * ３段階以上のアイコンを設定するには、強化デバフから連番でHP、MP、攻撃力、防御力、魔法力、魔法防御、敏捷性、運の順で設定します。
+ * 3段階以上のアイコンを設定するには、強化デバフから連番でHP、MP、攻撃力、防御力、魔法力、魔法防御、敏捷性、運の順で設定します。
+ * なお、他のプラグインとの競合から飛び番での設定は出来ませんので、必ず１段階目から最大までのアイコンを連続して並べてください。
  * 
  * 更新履歴
+ * 2023/3/8 Ver.1.0.1
+ * プラグインパラメータのスペルが間違っていたため修正。
  * 2022/2/11 Ver.1.0.0
  * 初版
  * 
@@ -100,14 +103,14 @@
  * @default 2
  * @min 1
  * 
- * @param MatkDebuffMaxLevel
+ * @param MatDebuffMaxLevel
  * @text 魔法力デバフ最大レベル
  * @desc 魔法力デバフ時の重ね掛けの最大レベル
  * @type number
  * @default 2
  * @min 1
  * 
- * @param MdffBuffMaxLevel
+ * @param MdfBuffMaxLevel
  * @text 魔法防御バフ最大レベル
  * @desc 魔法防御バフ時の重ね掛けの最大レベル
  * @type number
@@ -128,14 +131,14 @@
  * @default 2
  * @min 1
  * 
- * @param AgikDebuffMaxLevel
+ * @param AgiDebuffMaxLevel
  * @text 敏捷性デバフ最大レベル
  * @desc 敏捷性デバフ時の重ね掛けの最大レベル
  * @type number
  * @default 2
  * @min 1
  * 
- * @param LukfBuffMaxLevel
+ * @param LukBuffMaxLevel
  * @text 運バフ最大レベル
  * @desc 運バフ時の重ね掛けの最大レベル
  * @type number
@@ -169,8 +172,8 @@ buffParams[3] = Number(parameters['DefBuffMaxLevel'] || 2);
 debuffParams[3] = Number(parameters['DefDebuffMaxLevel'] || 2);
 buffParams[4] = Number(parameters['MatBuffMaxLevel'] || 2);
 debuffParams[4] = Number(parameters['MatDebuffMaxLevel'] || 2);
-buffParams[5] = Number(parameters['MatBuffMaxLevel'] || 2);
-debuffParams[5] = Number(parameters['MatDebuffMaxLevel'] || 2);
+buffParams[5] = Number(parameters['MdfBuffMaxLevel'] || 2);
+debuffParams[5] = Number(parameters['MdfDebuffMaxLevel'] || 2);
 buffParams[6] = Number(parameters['AgiBuffMaxLevel'] || 2);
 debuffParams[6] = Number(parameters['AgiDebuffMaxLevel'] || 2);
 buffParams[7] = Number(parameters['LukBuffMaxLevel'] || 2);
@@ -181,6 +184,7 @@ Game_BattlerBase.ICON_DEBUFF_START = DebuffIconIndex;
 
 
 Game_BattlerBase.prototype.isMaxBuffAffected = function(paramId) {//再定義
+    console.log(buffParams[paramId])
     return this._buffs[paramId] === buffParams[paramId];
 };
 

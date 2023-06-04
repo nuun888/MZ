0eb3c8ceda1590b1a66e0e3f79937019091aef16
nuun888 <70774024+nuun888@users.noreply.github.com>
2023-06-04 14:56:52 +0900

    Add files via upload

--- NUUN_AddAnimation.js
+++ NUUN_AddAnimation.js
@@ -10,7 +10,7 @@
  * @target MZ
  * @plugindesc Additional animation display
  * @author NUUN
- * @version 1.0.0
+ * @version 1.1.0
  * 
  * @help
  * Play multiple or additional item/skill animations.
@@ -18,6 +18,20 @@
  * Skill and item notes
  * <AddAnimation:[id],[id]...>
  * Play multiple animations simultaneously.
+ * [id]: Animation ID
+ * [Number]: Identification ID (integer) Omit the number in the first tag. Enter 2 for the second and subsequent items.
+ * 
+ * <AddAnimationWaitFrame[Number]:[WaitFrame]> 
+ * Delay playback of the above animation by the specified number of frames.
+ * [WaitFrame]; number of frames to delay If you enter -1, wait until the animation finishes playing.
+ * [Number]: Identification ID (integer) Omit the number in the first tag. Enter 2 for the second and subsequent items.
+ * 
+ * Example
+ * <AddAnimation:13>
+ * <AddAnimation2:14>
+ * <AddAnimationWaitFrame:30>
+ * <AddAnimationWaitFrame2:45>
+ * Animation ID 13 will be played 30 frames after the first animation is played, and animation ID 14 will be played after 45 frames.
  * 
  * <WaitAddAnimation:[id],[id]...>
  * Plays the next animation after one animation finishes.
@@ -26,6 +40,8 @@
  * This plugin is distributed under the MIT license.
  * 
  * Log
+ * 6/4/2023 Ver.1.1.0
+ * Added a function to delay playback by a specified number of frames.
  * 6/3/2023 Ver.1.0.0
  * First edition.
  * 
@@ -34,22 +50,37 @@
  * @target MZ
  * @plugindesc 追加アニメーション表示
  * @author NUUN
- * @version 1.0.0
+ * @version 1.1.0
  * 
  * @help
  * アイテム、スキルのアニメーションを複数または追加で再生させます。
  * 
  * スキル、アイテムのメモ欄
- * <AddAnimation:[id],[id]...>
+ * 
+ * <AddAnimation[Number]:[id],[id]...>
  * 複数のアニメーションを同時に再生します。
+ * [id]:アニメーションID
+ * [Number]:識別ID(整数) 最初のタグは数字を省略します。２番目以降は2と記入します。
+ * <AddAnimationWaitFrame[Number]:[WaitFrame]>　上記のアニメーションの再生を指定のフレーム数遅延再生します。
+ * [WaitFrame];遅延フレーム数 -1と記入した場合は、アニメーションが再生し終わるまで待ちます。
+ * [Number]:識別ID(整数) 最初のタグは数字を省略します。２番目以降は2と記入します。
+ * 例
+ * <AddAnimation:13>
+ * <AddAnimation2:14>
+ * <AddAnimationWaitFrame:30>
+ * <AddAnimationWaitFrame2:45>
+ * 最初のアニメーションが再生されて３０フレーム後にアニメーションID13が再生され、４５フレーム後にアニメーションID14番が再生されます。
  * 
  * <WaitAddAnimation:[id],[id]...>
  * 一つのアニメーションが終わった後に次のアニメーションを再生します。
+ * [id]:アニメーションID
  * 
  * 利用規約
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2023/6/4 Ver 1.1.0
+ * 指定のフレーム数遅延して再生できる機能を追加。
  * 2023/6/3 Ver 1.0.0
  * 初版
  * 
@@ -84,16 +115,20 @@ Imported.NUUN_AddAnimation = true;
         if (this._addAnimationList && this._addAnimationList.length > 0) {
             for (const data of this._addAnimationList) {
                 if (data.mode === 1) {
-                    this.push("waitAnimation");
+                    this.push("waitAnimation", 1, true);
+                } else if (data.mode === 0) {
+                    this.push("waitAnimation", data.wait, false);
                 }
                 this.push("showAnimation", data.subject, data.target, data.id);
             }
         }
     };
 
-    Window_BattleLog.prototype.waitAnimation = function() {
-        this._waitCount = 1;
-        $gameTemp.addAnimation = true;
+    Window_BattleLog.prototype.waitAnimation = function(wait, mode) {
+        this._waitCount = wait < 0 ? 1 : wait;
+        if (mode || wait < 0) {
+            $gameTemp.addAnimation = true;
+        }
     };
 
     Window_BattleLog.prototype.isWaitAnimation = function() {
@@ -108,11 +143,24 @@ Imported.NUUN_AddAnimation = true;
     Window_BattleLog.prototype.setAddAnimationList = function(item, subject, targets) {
         this._addAnimationList = [];
         let i = 0;
-        getAddAnimationList(item).forEach(animationId => {
-            this._addAnimationList.push({id: animationId, subject: subject, target: targets.clone(), mode: 0});
-        });
+        while (true) {
+            const tag = i === 0 ? "AddAnimation" : "AddAnimation" + (i + 1);
+            const tag2 = i === 0 ? "AddAnimationWaitFrame" : "AddAnimationWaitFrame" + (i + 1);
+            if (i === 0 && item.meta[tag]) {
+                getAddAnimationList(item, tag).forEach(animationId => {
+                    this._addAnimationList.push({id: animationId, subject: subject, target: targets.clone(), mode: 0, wait: getAddAnimationWaitFrame(item, tag2)});
+                });
+            } else if (item.meta[tag]) {
+                getAddAnimationList(item, tag).forEach(animationId => {
+                    this._addAnimationList.push({id: animationId, subject: subject, target: targets.clone(), mode: 0, wait: getAddAnimationWaitFrame(item, tag2)});
+                });
+            } else {
+                break;
+            }
+            i++;
+        }
         getWaitAddAnimationList(item).forEach(animationId => {
-            this._addAnimationList.push({id: animationId, subject: subject, target: targets.clone(), mode: 1});
+            this._addAnimationList.push({id: animationId, subject: subject, target: targets.clone(), mode: 1, wait: 0});
         });
     };
 
@@ -120,9 +168,12 @@ Imported.NUUN_AddAnimation = true;
         return item.meta.WaitAddAnimation ? item.meta.WaitAddAnimation.split(",").map(Number) : [];
     };
 
-    function getAddAnimationList(item) {
-        return item.meta.AddAnimation ? item.meta.AddAnimation.split(",").map(Number) : [];
+    function getAddAnimationList(item, tag) {
+        return item.meta[tag] ? item.meta[tag].split(",").map(Number) : [];
     };
 
-    
+    function getAddAnimationWaitFrame(item, tag) {
+        return item.meta[tag] ? item.meta[tag] : 0;
+    };
+
 })();

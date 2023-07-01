bbd1c1e109d9654cdbd4ea82ce5a1f15777b35e2
nuun888 <70774024+nuun888@users.noreply.github.com>
2023-07-01 18:41:52 +0900

    Add files via upload

--- NUUN_MultiElement.js
+++ NUUN_MultiElement.js
@@ -8,22 +8,61 @@
  */
 /*:
  * @target MZ
+ * @plugindesc Multiple Element
+ * @author NUUN
+ * @version 1.1.0
+ * 
+ * @help
+ * You can set multiple element for items and skills.
+ * 
+ * Item/skill notes
+ * <multElement:[id],[id],....> In addition to the database configuration elements, it has an elemen with element ID [id].
+ * <multElement:5> It has an element with element ID 5.
+ * <multElement:5,6> It has multiple elements with element IDs 5 and 6.
+ * 
+ * <multElementRate[id]:[rate]> Sets the probability of adding the specified element.
+ * [id]:Element ID
+ * [rate]:Added probability
+ * 
+ * Terms of Use
+ * This plugin is distributed under the MIT license.
+ * 
+ * Log
+ * 7/1/2023 Ver.1.1.0
+ * Added a function that allows you to set the probability for the element to be added.
+ * Changed the display in languages other than Japanese to English.
+ * 11/17/2021 Ver.1.0.1
+ * Fixed some processing.
+ * 8/8/2021 Ver.1.0.0
+ * First edition.
+ * 
+ */
+/*:ja
+ * @target MZ
  * @plugindesc 複数属性
  * @author NUUN
- * @version 1.0.1
+ * @version 1.1.0
  * 
  * @help
  * アイテム、スキルに複数の属性を設定できます。
  * 
  * アイテム、スキルのメモ欄
  * <multElement:[id],[id],....> データベースの設定属性に加えて属性ID[id]番の属性を持ちます。
+ * [id]:属性ID
  * <multElement:5> 属性ID5番の属性を持ちます。
  * <multElement:5,6> 属性ID5番と6番の複数属性を持ちます。
+ * 
+ * <multElementRate[id]:[rate]> 指定の属性を付加する確率を設定します。
+ * [id]:属性ID
+ * [rate]:付加される確率
  * 
  * 利用規約
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2023/7/1 Ver.1.1.0
+ * 付加する属性に確率を設定できる機能を追加。
+ * 日本語以外での表示を英語表示に変更。
  * 2021/11/7 Ver.1.0.1
  * 一部の処理の修正。
  * 2021/8/8 Ver.1.0.0
@@ -53,15 +92,27 @@ Game_Action.prototype.getItemElementsRate = function(target) {
 };
 
 Game_Action.prototype.getAttackElements = function() {
-  return this.getMultiElement().concat(this.subject().attackElements());
+    const elements = this.getMultiElement().concat(this.subject().attackElements());
+    return this.getFilterElements(elements);
 };
 
 Game_Action.prototype.getItemElements = function() {
-  return this.getMultiElement().concat(this.item().damage.elementId);
+    const elements = this.getMultiElement().concat(this.item().damage.elementId);
+    return this.getFilterElements(elements);
 };
 
 Game_Action.prototype.getMultiElement = function() {
   return this.item().meta.multElement ? this.item().meta.multElement.split(',').map(Number) : [];
+};
+
+Game_Action.prototype.getFilterElements = function(elements) {
+    const item = this.item();
+    const newElements = elements.filter(element => {
+        const tag = "multElementRate" + element;
+        return item.meta[tag] ? Math.randomInt(100) < Number(item.meta[tag]) : true;
+    });
+    this._multiElements = newElements;
+    return newElements;
 };
 
 })();

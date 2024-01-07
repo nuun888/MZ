35235598ad5c81a61c31e46863b28edde6a8412b
nuun888 <70774024+nuun888@users.noreply.github.com>
2024-01-07 15:38:18 +0900

    Add files via upload

--- NUUN_PlaybackRateBattleLog.js
+++ NUUN_PlaybackRateBattleLog.js
@@ -5,18 +5,80 @@
  * This software is released under the MIT License.
  * http://opensource.org/licenses/mit-license.php
  * -------------------------------------------------------------------------------------
- */ 
+ */
 /*:
+ * @target MZ
+ * @plugindesc Play rate battle log display
+ * @author NUUN
+ * @version 1.0.4
+ * @orderAfter BattleLogToMessage
+ * 
+ * @help
+ * HP, MP, TP regeneration rate damage and recovered regeneration amount will be displayed in the battle log.
+ * 
+ * Log
+ * 1/7/2024 Ver.1.0.4
+ * Corrected to display the replay log before the incapacity log when incapacitated due to slip damage.
+ * 8/25/2023 Ver.1.0.3
+ * Fixed an issue where the playback log was not displayed when used in conjunction with the battle log message display plugin.
+ * 6/22/2023 Ver.1.0.0
+ * Fixed an issue where the battle log was not cleared.
+ * 6/22/2023 Ver.1.0.0
+ * Fixed an issue where - was displayed in the recovery text.
+ * 1/21/2022 Ver.1.0.0
+ * First edition.
+ * 
+ * @param PlaybackRateHPRecoveryText
+ * @desc Text when recovering HP %1: Regenerated target %2: Regenerated amount
+ * @text Text when recovering HP
+ * @type string
+ * @default %1 is HP has been recovered by %2!
+ * 
+ * @param PlaybackRateHPDamageText
+ * @desc Text during HP damage %1: Regenerated target %2: Regenerated amount
+ * @text HP damage text
+ * @type string
+ * @default %1 is HP has decreased by %2!
+ * 
+ * @param PlaybackRateMPRecoveryText
+ * @desc Text when recovering MP %1: Regenerated target %2: Regenerated amount
+ * @text MP recovery text
+ * @type string
+ * @default %1 is MP has been recovered by %2!
+ * 
+ * @param PlaybackRateMPDamageText
+ * @desc Text during MP damage %1: Regenerated target %2: Regenerated amount
+ * @text MP damage text
+ * @type string
+ * @default %1 is MP has decreased by %2!
+ * 
+ * @param PlaybackRateTPRecoveryText
+ * @desc Text when recovering TP %1: Regenerated target %2: Regenerated amount
+ * @text TP recovery text
+ * @type string
+ * @default %1 is TP has accumulated %2!
+ * 
+ * @param PlaybackRateTPDamageText
+ * @desc Text during TP damage %1: Regenerated target %2: Regenerated amount
+ * @text TP damage text
+ * @type string
+ * @default %1 is TP has decreased by %2!
+ * 
+ * 
+ */
+/*:ja
  * @target MZ
  * @plugindesc 再生率バトルログ表示
  * @author NUUN
- * @version 1.0.3
+ * @version 1.0.4
  * @orderAfter BattleLogToMessage
  * 
  * @help
  * HP、MP、TP再生率でダメージ、回復した再生量をバトルログに表示させます。
  * 
  * 更新履歴
+ * 2024/1/7 Ver.1.0.4
+ * スリップダメージで戦闘不能になった時に、戦闘不能時のログよりも前に再生ログを表示するように修正。
  * 2023/8/25 Ver.1.0.3
  * バトルログのメッセージ表示プラグインとの併用で再生ログが表示されない問題を修正。
  * 2023/6/22 Ver.1.0.2
@@ -76,10 +138,10 @@ const PlaybackRateMPDamageText = String(parameters['PlaybackRateMPDamageText'] |
 const PlaybackRateTPRecoveryText = String(parameters['PlaybackRateTPRecoveryText'] || '%1のTPが%2溜まった！');
 const PlaybackRateTPDamageText = String(parameters['PlaybackRateTPDamageText'] || '%1のTPが%2減った！');
 
-const _Window_BattleLog_displayRegeneration = Window_BattleLog.prototype.displayRegeneration;
-Window_BattleLog.prototype.displayRegeneration = function(subject) {
-    _Window_BattleLog_displayRegeneration.call(this, subject);
-    this.displayPlaybackRate(subject);
+const _BattleManager_displayBattlerStatus = BattleManager.displayBattlerStatus;
+BattleManager.displayBattlerStatus = function(battler, current) {
+    this._logWindow.displayPlaybackRate(battler);
+    _BattleManager_displayBattlerStatus.call(this, battler, current);
 };
 
 Window_BattleLog.prototype.displayPlaybackRate = function(subject) {
@@ -116,7 +178,6 @@ Window_BattleLog.prototype.displayPlaybackRate = function(subject) {
         }
         //this.push("performPlaybackRate", subject);
         this.push("wait");
-        this.push("clear");
     }
 };
 

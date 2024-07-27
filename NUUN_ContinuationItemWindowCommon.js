70b7feb7a3677ff621ef36db5226b967a3d4064b
nuun888 <70774024+nuun888@users.noreply.github.com>
2024-07-27 17:07:14 +0900

    Add files via upload

--- NUUN_ContinuationItemWindowCommon.js
+++ NUUN_ContinuationItemWindowCommon.js
@@ -12,12 +12,15 @@
  * @author NUUN
  * @base NUUN_Base
  * @orderAfter NUUN_Base
- * @version 1.0.1
+ * @version 1.1.0
  * 
  * @help
  * Items that perform common events with use effects, prevent the screen from closing when using skills.
  * Set the common event ID that does not close the screen with the use effect in "Screen continuation common event" of the plug-in parameter. Multiple common events can be set.
  * When a common event not on the list is executed with a use effect, the screen closes normally.
+ * 
+ * Item Notes
+ * <CommonContinuation>: This item will not close the window even if a common event is executed.
  * 
  * Specification
  * Waiting for event commands is not supported.
@@ -26,6 +29,8 @@
  * This plugin is distributed under the MIT license.
  * 
  * Log
+ * 7/27/2024 Ver.1.1.0
+ * Added a function that allows you to set an item that will not close the menu even when a common event is executed from a specified item.
  * 7/11/2023 Ver.1.0.1
  * Processing fixes.
  * 7/9/2023 Ver.1.0.0
@@ -44,12 +49,15 @@
  * @author NUUN
  * @base NUUN_Base
  * @orderAfter NUUN_Base
- * @version 1.0.1
+ * @version 1.1.0
  * 
  * @help
  * 使用効果でコモンイベントを実行するアイテム、スキルを使用したときに画面を閉じないようにします。
  * プラグインパラメータの画面継続コモンイベントで使用効果で画面を閉じないコモンイベントIDを設定します。コモンイベントは複数設定できます。
  * リストにないコモンイベントが使用効果で実行されたときは、通常通り画面が閉じます。
+ * 
+ * アイテムのメモ欄
+ * <CommonContinuation>:このアイテムはコモンイベントを実行してもウィンドウが閉じなくなります。
  * 
  * 仕様
  * イベントコマンドのウエイトは非対応です。
@@ -58,6 +66,8 @@
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2024/7/27 Ver.1.1.0
+ * 指定のアイテムから実行したコモンイベントでもメニューを閉じないアイテムを設定できる機能を追加。
  * 2023/7/11 Ver.1.0.1
  * 処理の修正。
  * 2023/7/9 Ver.1.0.0
@@ -82,7 +92,7 @@ Imported.NUUN_ContinuationItemWindowCommon = true;
     Scene_ItemBase.prototype.checkCommonEvent = function() {
         if ($gameTemp.isCommonEventReserved()) {
             $gameTemp._commonEventQueue.forEach((commonEventId, index) => {
-                if (isContinuationCommonEvent(commonEventId)) {
+                if (this.isContinuationCommonEvent(commonEventId)) {
                     this.continuationCommonEvent(commonEventId, index);
                 }
             });
@@ -103,9 +113,9 @@ Imported.NUUN_ContinuationItemWindowCommon = true;
         }
     };
 
-    function isContinuationCommonEvent(commonEventId) {
-        return SceneContinuationCommonEvent.some(id => id === commonEventId);
+    Scene_ItemBase.prototype.isContinuationCommonEvent = function(commonEventId) {
+        const item = this.item();
+        return (item && item.meta.CommonContinuation) || SceneContinuationCommonEvent.some(id => id === commonEventId);
     };
-   
-    
+
 })();

1b405e4f0919f6e00d454f112ce26f68a9ea45d2
nuun888 <70774024+nuun888@users.noreply.github.com>
2024-10-28 19:34:07 +0900

    Add files via upload

--- NUUN_StatusParamEX.js
+++ NUUN_StatusParamEX.js
@@ -11,7 +11,9 @@
  * @target MZ
  * @plugindesc アクターステータスの最大値設定
  * @author NUUN
- * @version 1.0.2
+ * @base NUUN_Base
+ * @orderAfter NUUN_Base
+ * @version 1.0.3
  *            
  * @help 
  * アクターの能力値に上限を授けます。デフォルトでは能力値の上限はInfinity（無限）
@@ -25,10 +27,11 @@
  * <MaxLimitTP:[param]> 最大TP
  * <MaxLimitATK:[param]> 最大攻撃力
  * <MaxLimitDEF:[param]> 最大防御力
- * <MaxLimitMAG:[param]> 最大魔法力
- * <MaxLimitMAT:[param]> 最大魔法防御
+ * <MaxLimitMAT:[param]> 最大魔法力
+ * <MaxLimitMDF:[param]> 最大魔法防御
  * <MaxLimitAGI:[param]> 最大敏捷性
  * <MaxLimitLUK:[param]> 最大運
+ * ※[param]に記入する数値は、[]は外して数値のみ記入して下さい。
  * 
  * 複数設定されている場合は、一番最大値が高い値が適用されます。
  * 
@@ -36,6 +39,8 @@
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2024/10/28 Ver.1.0.3
+ * 最大魔法力、最大魔法防御の設定方法の誤字を修正。
  * 2022/1/22 Ver.1.0.2
  * 戦闘中にエラーが出る問題を修正。
  * 2022/1/22 Ver.1.0.1
@@ -112,93 +117,88 @@ var Imported = Imported || {};
 Imported.NUUN_StatusParamEX = true;
 
 (() => {
-  const parameters = PluginManager.parameters('NUUN_StatusParamEX');
-  let limitHP = Number(parameters['limitHP'] || -1);
-  let limitMP = Number(parameters['limitMP'] || -1);
-  let limitTP = Number(parameters['limitTP'] || -1);
-  let limitAtk = Number(parameters['limitAtk'] || -1);
-  let limitDef = Number(parameters['limitDef'] || -1);
-  let limitMat = Number(parameters['limitMat'] || -1);
-  let limitMdf = Number(parameters['limitMdf'] || -1);
-  let limitAgi = Number(parameters['limitAgi'] || -1);
-  let limitLuk = Number(parameters['limitLuk'] || -1);
+    const params = Nuun_PluginParams.getPluginParams(document.currentScript);
 
-  const _Game_Actor_paramMax = Game_Actor.prototype.paramMax;
-  Game_Actor.prototype.paramMax = function(paramId) {
-    let param = 0;
-    if(paramId === 0){
-      param = this.hpMaxParam();
-    } else if (paramId === 1) {
-      param = this.mpMaxParam();
-    } else if (paramId === 2) {
-      param = this.atkMaxParam();
-    } else if (paramId === 3) {
-      param = this.defMaxParam();
-    } else if (paramId === 4) {
-      param = this.matMaxParam();
-    } else if (paramId === 5) {
-      param = this.mdfMaxParam();
-    } else if (paramId === 6) {
-      param = this.agiMaxParam();
-    } else if (paramId === 7) {
-      param = this.lukMaxParam();
-    }
-    return param >= 0 ? param : _Game_Actor_paramMax.call(this, paramId);
-  };
+    const _Game_Actor_paramMax = Game_Actor.prototype.paramMax;
+    Game_Actor.prototype.paramMax = function(paramId) {
+        let param = 0;
+        if(paramId === 0){
+            param = this.hpMaxParam();
+        } else if (paramId === 1) {
+            param = this.mpMaxParam();
+        } else if (paramId === 2) {
+            param = this.atkMaxParam();
+        } else if (paramId === 3) {
+            param = this.defMaxParam();
+        } else if (paramId === 4) {
+            param = this.matMaxParam();
+        } else if (paramId === 5) {
+            param = this.mdfMaxParam();
+        } else if (paramId === 6) {
+            param = this.agiMaxParam();
+        } else if (paramId === 7) {
+            param = this.lukMaxParam();
+        }
+        return param >= 0 ? param : _Game_Actor_paramMax.call(this, paramId);
+    };
 
-  const _Game_BattlerBase_maxTp = Game_BattlerBase.prototype.maxTp;
-  Game_BattlerBase.prototype.maxTp = function() {
-    if (this.isActor()) {
-      const param = this.tpMaxParam();
-      return param >= 0 ? param : _Game_BattlerBase_maxTp.call(this);
-    }
-    return _Game_BattlerBase_maxTp.call(this);
-  };
-
-  Game_Actor.prototype.tpMaxParam = function() {
-    return Math.max(this.classMaxData('TP'), this.actorMaxData('TP'), limitTP);
-  };
-
-  Game_Actor.prototype.hpMaxParam = function() {
-    return Math.max(this.classMaxData('HP'), this.actorMaxData('HP'), limitHP);
-  };
-
-  Game_Actor.prototype.mpMaxParam = function() {
-    return Math.max(this.classMaxData('MP'), this.actorMaxData('MP'), limitMP);
-  };
-
-  Game_Actor.prototype.atkMaxParam = function() {
-    return Math.max(this.classMaxData('ATK'), this.actorMaxData('ATK'), limitAtk);
-  };
-
-  Game_Actor.prototype.defMaxParam = function() {
-    return Math.max(this.classMaxData('DEF'), this.actorMaxData('DEF'), limitDef);
-  };
-
-  Game_Actor.prototype.matMaxParam = function() {
-    return Math.max(this.classMaxData('MAT'), this.actorMaxData('MAT'), limitMat);
-  };
-
-  Game_Actor.prototype.mdfMaxParam = function() {
-    return Math.max(this.classMaxData('MDF'), this.actorMaxData('MDF'), limitMdf);
-  };
-
-  Game_Actor.prototype.agiMaxParam = function() {
-    return Math.max(this.classMaxData('AGI'), this.actorMaxData('AGI'), limitAgi);
-  };
-
-  Game_Actor.prototype.lukMaxParam = function() {
-    return Math.max(this.classMaxData('LUK'), this.actorMaxData('LUK'), limitLuk);
-  };
-
-  Game_Actor.prototype.classMaxData = function(type) {
-    const tag = 'MaxLimit'
-    return this.currentClass().meta[tag + type] || -1;
-  };
-
-  Game_Actor.prototype.actorMaxData = function(type) {
-    const tag = 'MaxLimit'
-    return this.actor().meta[tag + type] || -1;
-  };
+    const _Game_BattlerBase_maxTp = Game_BattlerBase.prototype.maxTp;
+    Game_BattlerBase.prototype.maxTp = function() {
+        if (this.isActor()) {
+            const param = this.tpMaxParam();
+            return param >= 0 ? param : _Game_BattlerBase_maxTp.call(this);
+        }
+        return _Game_BattlerBase_maxTp.call(this);
+    };
+  
+    Game_Actor.prototype.tpMaxParam = function() {
+      return Math.max(this.classMaxData('TP'), this.actorMaxData('TP'), params.limitTP);
+    };
+  
+    Game_Actor.prototype.hpMaxParam = function() {
+      return Math.max(this.classMaxData('HP'), this.actorMaxData('HP'), params.limitHP);
+    };
+  
+    Game_Actor.prototype.mpMaxParam = function() {
+      return Math.max(this.classMaxData('MP'), this.actorMaxData('MP'), params.limitMP);
+    };
+  
+    Game_Actor.prototype.atkMaxParam = function() {
+      return Math.max(this.classMaxData('ATK'), this.actorMaxData('ATK'), params.limitAtk);
+    };
+  
+    Game_Actor.prototype.defMaxParam = function() {
+      return Math.max(this.classMaxData('DEF'), this.actorMaxData('DEF'), params.limitDef);
+    };
+  
+    Game_Actor.prototype.matMaxParam = function() {
+      return Math.max(this.classMaxData('MAT'), this.actorMaxData('MAT'), params.limitMat);
+    };
+  
+    Game_Actor.prototype.mdfMaxParam = function() {
+      return Math.max(this.classMaxData('MDF'), this.actorMaxData('MDF'), params.limitMdf);
+    };
+  
+    Game_Actor.prototype.agiMaxParam = function() {
+      return Math.max(this.classMaxData('AGI'), this.actorMaxData('AGI'), params.limitAgi);
+    };
+  
+    Game_Actor.prototype.lukMaxParam = function() {
+      return Math.max(this.classMaxData('LUK'), this.actorMaxData('LUK'), params.limitLuk);
+    };
+  
+    Game_Actor.prototype.classMaxData = function(type) {
+      const tag = 'MaxLimit' + type;
+      return this.currentClass().meta[tag] || -1;
+      //return NuunManager.stringCodeSplit(this.currentClass().meta[tag], true) || -1;
+      
+    };
+  
+    Game_Actor.prototype.actorMaxData = function(type) {
+      const tag = 'MaxLimit' + type;
+      return this.actor().meta[tag] || -1;
+      //return NuunManager.stringCodeSplit(this.actor().meta[tag], true) || -1;
+      
+    };
 
 })();

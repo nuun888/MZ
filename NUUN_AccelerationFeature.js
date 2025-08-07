92fa1a0c0732fd9253a4691e5fcd1b75d980b5eb
nuun888 <70774024+nuun888@users.noreply.github.com>
2025-08-08 06:43:40 +0900

    Add files via upload

--- NUUN_AccelerationFeature.js
+++ NUUN_AccelerationFeature.js
@@ -10,7 +10,7 @@
  * @target MZ
  * @plugindesc Ability value increase/decrease feature per turn
  * @author NUUN
- * @version 2.0.2
+ * @version 2.0.3
  * 
  * @help
  * You can set the feature that the ability value gradually increases for each turn.
@@ -28,6 +28,8 @@
  * 
  * 
  * Log
+ * 8/8/2025 Ver.2.0.3
+ * Fixed an issue where abilities would increase while moving.
  * 11/25/2022 Ver.2.0.2
  * Changed the display in languages other than Japanese to English.
  * 1/26/2022 Ver.2.0.1
@@ -45,7 +47,7 @@
  * @target MZ
  * @plugindesc 能力値ターン毎増減特徴
  * @author NUUN
- * @version 2.0.2
+ * @version 2.0.3
  * 
  * @help
  * ターンごとに徐々に能力値が上昇していく特徴を設定できます。
@@ -61,6 +63,8 @@
  * このプラグインはMITライセンスで配布しています。
  * 
  * 更新履歴
+ * 2025/8/8 Ver.2.0.3
+ * 移動中に能力が上昇してしまう問題を修正。
  * 2022/11/25 Ver.2.0.2
  * 日本語以外での表示を英語表示に変更。
  * 2022/1/26 Ver.2.0.1
@@ -84,92 +88,93 @@ const parameters = PluginManager.parameters('NUUN_AccelerationFeature');
 
 const _Game_Battler_initMembers = Game_Battler.prototype.initMembers;
 Game_Battler.prototype.initMembers = function() {
-  _Game_Battler_initMembers.call(this);
-  this.clearAccelerationbRate();
+    _Game_Battler_initMembers.call(this);
+    this.clearAccelerationbRate();
 };
 
 Game_BattlerBase.prototype.clearAccelerationbRate = function() {
-  //this._accelerationb 旧
-  this._abilityRate = [-1,-1,-1,-1,-1,-1,-1,-1];
+    //this._accelerationb 旧
+    this._abilityRate = [-1,-1,-1,-1,-1,-1,-1,-1];
 };
 
 Game_BattlerBase.prototype.resetAccelerationbRate = function(paramId) {
-  if (!this._abilityRate) {
-    this.clearAccelerationbRate();
-  }
-  this._abilityRate[paramId] = -1;
+    if (!this._abilityRate) {
+        this.clearAccelerationbRate();
+    }
+    this._abilityRate[paramId] = -1;
 };
 
 Game_BattlerBase.prototype.setAccelerationbRate = function(paramId, rate) {
-  if (!this._abilityRate) {
-    this.clearAccelerationbRate();
-  }
-  const param = this._abilityRate[paramId];
-  this._abilityRate[paramId] = param < 0 ? 1.0 + rate : Math.max(param + rate, 0);
+    if (!this._abilityRate) {
+        this.clearAccelerationbRate();
+    }
+    const param = this._abilityRate[paramId];
+    this._abilityRate[paramId] = param < 0 ? 1.0 + rate : Math.max(param + rate, 0);
 };
 
 Game_BattlerBase.prototype.getAccelerationbRate = function(paramId) {
-  if (!this._abilityRate) {
-    this.clearAccelerationbRate();
-  }
-  return this._abilityRate[paramId] >= 0 ? this._abilityRate[paramId] : 1.0;
+    if (!this._abilityRate) {
+        this.clearAccelerationbRate();
+    }
+    return this._abilityRate[paramId] >= 0 ? this._abilityRate[paramId] : 1.0;
 };
 
 Game_BattlerBase.prototype.traitAccelerationbRate = function(paramId) {
-  const rates = this.traitAccelerationb(paramId);
-  if (rates.length > 0) {
-    return rates.reduce((r, rate) => {
-      return r + rate / 100;
-    }, 0.0);
-  } else {
-    return -1;
-  }
+    const rates = this.traitAccelerationb(paramId);
+    if (rates.length > 0) {
+        return rates.reduce((r, rate) => {
+        return r + rate / 100;
+        }, 0.0);
+    } else {
+        return -1;
+    }
 };
 
 Game_BattlerBase.prototype.traitAccelerationb = function(paramId) {
-  const tag = 'Accelerationb';
-  return this.traitObjects().reduce((r, trait) => {
-    if (trait.meta[tag + paramId]) {
-      return r.concat(Number(trait.meta[tag + paramId]));
-    } else {
-      return r;
-    }
-  }, []);
+    const tag = 'Accelerationb';
+    return this.traitObjects().reduce((r, trait) => {
+        if (trait.meta[tag + paramId]) {
+        return r.concat(Number(trait.meta[tag + paramId]));
+        } else {
+        return r;
+        }
+    }, []);
 };
 
 Game_BattlerBase.prototype.setAccelerationbRates = function() {
-  for (let i = 0; i < 8; i++) {
-    const rate = this.traitAccelerationbRate(i);
-    if (rate >= 0) {
-      this.setAccelerationbRate(i, rate);
-    } else {
-      this.resetAccelerationbRate(i);
+    if (!$gameParty.inBattle()) return;//移動中は実行しない
+    for (let i = 0; i < 8; i++) {
+        const rate = this.traitAccelerationbRate(i);
+        if (rate >= 0) {
+            this.setAccelerationbRate(i, rate);
+        } else {
+            this.resetAccelerationbRate(i);
+        }
     }
-  }
 };
 
 const _Game_BattlerBase_paramRate = Game_BattlerBase.prototype.paramRate;
 Game_BattlerBase.prototype.paramRate = function(paramId) {
-  const rate = _Game_BattlerBase_paramRate.call(this, paramId);
-  return rate * this.getAccelerationbRate(paramId);
+    const rate = _Game_BattlerBase_paramRate.call(this, paramId);
+    return rate * this.getAccelerationbRate(paramId);
 };
 
 const _Game_Battler_onBattleStart = Game_Battler.prototype.onBattleStart;
 Game_Battler.prototype.onBattleStart = function(advantageous) {
-  _Game_Battler_onBattleStart.call(this, advantageous);
-  this.clearAccelerationbRate();
+    _Game_Battler_onBattleStart.call(this, advantageous);
+    this.clearAccelerationbRate();
 };
 
 const _Game_Battler_onTurnEnd =Game_Battler.prototype.onTurnEnd;
 Game_Battler.prototype.onTurnEnd = function() {
-  _Game_Battler_onTurnEnd.call(this);
-  this.setAccelerationbRates();
+    _Game_Battler_onTurnEnd.call(this);
+    this.setAccelerationbRates();
 };
 
 const _Game_Actor_onBattleEnd = Game_Actor.prototype.onBattleEnd;
 Game_Actor.prototype.onBattleEnd = function() {
-  _Game_Actor_onBattleEnd.call(this)
-  this.clearAccelerationbRate();
+    _Game_Actor_onBattleEnd.call(this)
+    this.clearAccelerationbRate();
 };
 
 })();

a28f376ae3c48a6e598ccf56e0908fd3aefc65ce
nuun888 <70774024+nuun888@users.noreply.github.com>
2025-09-20 02:36:09 +0900

    Add files via upload

--- NUUN_FastTravel.js
+++ NUUN_FastTravel.js
@@ -42,8 +42,12 @@
  * 
  *  
  * Terms of Use
- * This plugin is distributed under the MIT license.
- * 
+ * Credit: Optional
+ * Commercial use: Possible
+ * Adult content: Possible
+ * Modifications: Possible
+ * Redistribution: Possible
+ * Support is not available for modified versions or downloads from sources other than https://github.com/nuun888/MZ, the official forum, or authorized retailers.
  * 
  * Log
  * 5/17/2025 Ver.1.1.1
@@ -438,8 +442,12 @@
  * 
  *  
  * 利用規約
- * このプラグインはMITライセンスで配布しています。
- * 無償、有償ゲーム問わずご利用いただけます。
+ * クレジット表記：任意
+ * 商業利用：可能
+ * 成人向け：可能
+ * 改変：可能
+ * 再配布：可能
+ * https://github.com/nuun888/MZ、公式フォーラム、正規販売サイト以外からのダウンロード、改変済みの場合はサポートは対象外となります。
  * 
  * 
  * 更新履歴

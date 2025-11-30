166fca5f4f40bb9607a0587cbebc8e95ce80ff1c
nuun888 <70774024+nuun888@users.noreply.github.com>
2025-11-30 18:50:49 +0900

    Add files via upload

--- NUUN_TraitsPlusNuunTraitObjects.js
+++ NUUN_TraitsPlusNuunTraitObjects.js
@@ -2,8 +2,6 @@
  * NUUN_TraitsPlusNuunTraitObjects.js
  * 
  * Copyright (C) 2025 NUUN
- * This software is released under the MIT License.
- * http://opensource.org/licenses/mit-license.php
  * -------------------------------------------------------------------------------------
  */
 /*:
@@ -13,7 +11,7 @@
  * @base NUUN_Base
  * @orderAfter NUUN_Base
  * @orderAfter NRP_TraitsPlus
- * @version 1.0.0
+ * @version 1.0.1
  * 
  * @help
  * This is a plugin for applying ability parameters based on NRP_TraitsPlus and unique characteristics.
@@ -23,9 +21,17 @@
  * Equipment set bonus (NUUN_SetBonusEquip)
  * 
  * Terms of Use
- * This plugin is distributed under the MIT license.
+ * Credit: Optional
+ * Commercial use: Possible
+ * Adult content: Possible
+ * Modifications: Possible
+ * Redistribution: Possible
+ * Support is not available for modified versions or downloads from sources other than https://github.com/nuun888/MZ, the official forum, or authorized retailers.
  * 
  * Log
+ * 11/30/2025 Ver.1.0.1
+ * Fixed an issue that caused an error to occur in Ver.1.5.9 and later.
+ * Fixed so that passive skills can be applied to enemies as well.
  * 1/31/2025 Ver.1.0.0
  * First edition.
  * 
@@ -38,7 +44,7 @@
  * @base NUUN_Base
  * @orderAfter NUUN_Base
  * @orderAfter NRP_TraitsPlus
- * @version 1.0.0
+ * @version 1.0.1
  * 
  * @help
  * NRP_TraitsPlusと独自の特徴による能力値のパラメータを適用させるためのプラグインです。
@@ -48,9 +54,17 @@
  * 装備セットボーナス(NUUN_SetBonusEquip)
  * 
  * 利用規約
- * このプラグインはMITライセンスで配布しています。
+ * クレジット表記：任意
+ * 商業利用：可能
+ * 成人向け：可能
+ * 改変：可能
+ * 再配布：可能
+ * https://github.com/nuun888/MZ、公式フォーラム、正規販売サイト以外からのダウンロード、改変済みの場合はサポートは対象外となります。
  * 
  * 更新履歴
+ * 2025/11/30 Ver.1.0.1
+ * Ver.1.5.9以降エラーが出る問題を修正。
+ * 敵にもパッシブスキルが適用できるように修正。
  * 2025/1/31 Ver.1.0.0
  * 初版
  * 
@@ -66,12 +80,29 @@ Imported.NUUN_TraitsPlusNuunTraitObjects = true;
     Game_Actor.prototype.traitBattlerObjects = function() {
         let objects = _Game_Actor_traitBattlerObjects.apply(this, arguments);
         if (Imported.NUUN_PassiveSkill) {//条件パッシブスキル
-            Array.prototype.push.apply(objects, this.passiveObject());
+            Array.prototype.push.apply(objects, this.traitBattlerPassiveObject());
         }
         if (Imported.NUUN_SetBonusEquip) {//セットボーナス
             Array.prototype.push.apply(objects, this.setBonusObject());
         }
         return objects;
+    };
+
+    const _Game_Enemy_traitBattlerObjects = Game_Enemy.prototype.traitBattlerObjects;
+    Game_Enemy.prototype.traitBattlerObjects = function() {
+        let objects = _Game_Enemy_traitBattlerObjects.apply(this, arguments);
+        if (Imported.NUUN_PassiveSkill) {//条件パッシブスキル
+            Array.prototype.push.apply(objects, this.traitBattlerPassiveObject());
+        }
+        return objects;
+    };
+
+    Game_Battler.prototype.traitBattlerPassiveObject = function() {
+        if (!!this.passiveObject) {
+            return this.passiveObject();
+        } else {
+            return this.passiveSkillObject();
+        }
     };
     
 })();

c4d87a0c19d3d2196c31dd4011d9bbec7120ff85
nuun888 <70774024+nuun888@users.noreply.github.com>
2026-06-14 19:42:48 +0900

    Add files via upload

--- NUUN_SystemDatabaseUnlimited.js
+++ NUUN_SystemDatabaseUnlimited.js
@@ -2,17 +2,13 @@
  * NUUN_SystemDatabaseUnlimited.js
  * 
  * Copyright (C) 2023 NUUN
- * This software is released under the MIT License.
- * http://opensource.org/licenses/mit-license.php
  * -------------------------------------------------------------------------------------
  */
 /*:
  * @target MZ
  * @plugindesc Exceeded database limit
  * @author NUUN
- * @base NUUN_Base
- * @orderAfter NUUN_Base
- * @version 1.0.2
+ * @version 1.1.0
  * 
  * @help
  * You can set more than the maximum number of maps (default 2000) and the maximum number of animations (default 1000).
@@ -20,9 +16,16 @@
  * It is recommended to make a backup before execution.
  * 
  * Terms of Use
- * This plugin is distributed under the MIT license.
+ * Credit: Optional
+ * Commercial use: Possible
+ * Adult content: Possible
+ * Modifications: Possible
+ * Redistribution: Possible
+ * Support is not available for modified versions or downloads from sources other than https://github.com/nuun888/MZ, the official forum, or authorized retailers.
  * 
  * Log
+ * 6/14/2023 Ver.1.1.0
+ * Modified to run without "NUUN Base".
  * 8/23/2023 Ver.1.0.2
  * Fixed to output a map JSON file when adding a new map.
  * 1/21/2023 Ver.1.0.1
@@ -59,9 +62,7 @@
  * @target MZ
  * @plugindesc データベース上限突破
  * @author NUUN
- * @base NUUN_Base
- * @orderAfter NUUN_Base
- * @version 1.0.2
+ * @version 1.1.0
  * 
  * @help
  * マップの上限数(デフォルト2000)、アニメーションの上限数(デフォルト1000)を超えて設定できるようになります。
@@ -69,9 +70,16 @@
  * 実行前にバックアップをとることを推奨いたします。
  * 
  * 利用規約
- * このプラグインはMITライセンスで配布しています。
+ * クレジット表記：任意
+ * 商業利用：可能
+ * 成人向け：可能
+ * 改変：可能
+ * 再配布：可能
+ * https://github.com/nuun888/MZ、公式フォーラム、正規販売サイト以外からのダウンロード、改変済みの場合はサポートは対象外となります。
  * 
  * 更新履歴
+ * 6/14/2026 Ver.1.1.0
+ * NUUN_Baseなしで実行できるように修正。
  * 2023/8/23 Ver.1.0.2
  * マップ新規追加時にマップJSONファイルを出力するように修正。
  * 2023/1/21 Ver.1.0.1
@@ -110,19 +118,185 @@ var Imported = Imported || {};
 Imported.NUUN_SystemDatabaseUnlimited = true;
 
 (() => {
-    const parameters = PluginManager.parameters('NUUN_SystemDatabaseUnlimited');
-    const pluginName = 'NUUN_SystemDatabaseUnlimited';
+    class Nuun_PluginParams_SystemDatabaseUnlimited {
+        static getPluginParams(text) {//document.currentScript
+            try {
+                const name = String(Utils.extractFileName(text.src).split('.').shift());
+                const params = PluginManager.parameters(name);
+                if (params) {
+                    const pluginParam = new Nuun_PluginParamData(params);
+                    pluginParam.setPluginName(name);
+                    return pluginParam.getParameters();
+                }
+                return {pluginName: name};
+            } catch (error) {
+                const log = ($gameSystem.isJapanese() ? "コアスクリプトをVer.1.3.2以降に更新してください。" : "Please update the core script to version 1.3.2 or later.");
+                throw ["ParameterError", log];
+            }
+        }
+    };
+
+    class Nuun_PluginParamData {
+        constructor(text) {
+            this._parameters = JSON.parse(JSON.stringify(text, this._convertParams)) || {};
+        }
+
+        _convertParams(key, code) {
+            try {
+                return JSON.parse(code);
+            } catch (e) {
+                if (isNaN(code)) {
+                    if (!code) {
+                        return null;
+                    }
+                    try {
+                        if (code.indexOf("'") === 0 || code.indexOf('"' === 0)) {
+                            return eval(code);//'または"を外す。
+                        }
+                        return !!code ? String(code) : null;
+                    } catch (e) {
+                        if (typeof {} === "object") {
+                            return code;
+                        }
+                        return !!code ? String(code) : null;
+                    }
+                } else {
+                    return String(code);
+                }
+            }
+        }
+
+        getParameters() {
+            return this._parameters;
+        }
+
+        setPluginName(name) {
+            this._parameters.pluginName = name;
+        }
+
+
+        getMetaTag(object, code) {
+            const data = object.meta[code];
+            let list = [];
+            if (data !== undefined) {
+                try {
+                    list = data.split(',');
+                } catch (error) {
+                    return this.getTextCodeMeta(data);
+                }
+                list.forEach(a => {
+                    a = this.getTextCodeMeta(a);
+                });
+                return list;
+            } else {
+                return undefined;
+            }
+        }
+
+        getTextCodeMeta(text) {
+            if (isNaN(text)) {
+                return text;
+            } else {
+                return Number(text);
+            }
+        }
+    };
+
+    const params = Nuun_PluginParams_SystemDatabaseUnlimited.getPluginParams(document.currentScript);
+    const pluginName = params.pluginName;
+
+    function NuunSystemDatabaseUnlimitedManager() {
+        throw new Error("This is a static class");
+    }
+
+    NuunSystemDatabaseUnlimitedManager.addMap = function(dataPass) {
+        try {
+            const dataFile = getSystemData(dataPass);
+            const length = dataFile.length;
+            while (true) {
+                if (!dataFile[length]) {
+                    break;
+                }
+                length++;
+            }
+            this.outputMapJson(length);
+            dataFile.push(getMapSystemData(length));
+            const fs = require("fs");
+            const pass = "data/" + dataPass;
+            fs.writeFileSync(pass, JSON.stringify(dataFile));
+            const log = $gameSystem.isJapanese() ? "出力が完了しました。" : "Output completed. ";
+            console.log(log);
+        } catch (e) {
+            const elog = $gameSystem.isJapanese() ? "エラーが発生しました。" : "An error has occurred";
+            console.log(elog);
+        }
+    };
+
+    NuunSystemDatabaseUnlimitedManager.addMapSystemData = function(dataPass, MaxDatabase) {
+        try {
+            const dataFile = getSystemData(dataPass);
+            const length = dataFile.length;
+            if (MaxDatabase > 0) {
+                for (let i = 0; i <= MaxDatabase; i++) {
+                    if (!dataFile[i]) {
+                        dataFile[i] = getMapSystemData(i);
+                        this.outputMapJson(i);
+                    }
+                }
+            }
+            const fs = require("fs");
+            const pass = "data/" + dataPass;
+            fs.writeFileSync(pass, JSON.stringify(dataFile));
+            const log = $gameSystem.isJapanese() ? "出力が完了しました。" : "Output completed. ";
+            console.log(log);
+        } catch (e) {
+            const elog = $gameSystem.isJapanese() ? "エラーが発生しました。" : "An error has occurred";
+            console.log(elog);
+        }
+    };
+
+    NuunSystemDatabaseUnlimitedManager.addSystemData = function(dataPass, MaxDatabase) {
+        try {
+            const dataFile = getSystemData(dataPass);
+            const length = dataFile.length;
+            if (MaxDatabase > 0) {
+                for (let i = 0; i <= MaxDatabase; i++) {
+                    if (!dataFile[i]) {
+                        dataFile[i] = getAnimationsSystemData(i);
+                    }
+                }
+            }
+            const fs = require("fs");
+            const pass = "data/" + dataPass;
+            fs.writeFileSync(pass, JSON.stringify(dataFile));
+            const log = $gameSystem.isJapanese() ? "出力が完了しました。" : "Output completed. ";
+            console.log(log);
+        } catch (e) {
+            const elog = $gameSystem.isJapanese() ? "エラーが発生しました。" : "An error has occurred";
+            console.log(elog);
+        }
+    };
+
+    NuunSystemDatabaseUnlimitedManager.outputMapJson = function(index) {
+        const filename = "Map%1".format(index.padZero(3));
+        const dataFile = getMapData();
+        const fs = require("fs");
+        const pass = "data/" + filename + ".json";
+        fs.writeFileSync(pass, JSON.stringify(dataFile));
+    };
+
     PluginManager.registerCommand(pluginName, 'DataBaseAddAnimations', args => {
-        NuunManager.addSystemData('Animations.json', Number(args.MaxDatabase));
+        NuunSystemDatabaseUnlimitedManager.addSystemData('Animations.json', Number(args.MaxDatabase));
     });
 
     PluginManager.registerCommand(pluginName, 'DataBaseAddMaps', args => {
-        NuunManager.addMapSystemData('MapInfos.json', Number(args.MaxDatabase), eval(args.CreateMapJson));
+        NuunSystemDatabaseUnlimitedManager.addMapSystemData('MapInfos.json', Number(args.MaxDatabase));
     });
 
     PluginManager.registerCommand(pluginName, 'AddMap', args => {
-        NuunManager.addMap('MapInfos.json', eval(args.CreateMapJson));
+        NuunSystemDatabaseUnlimitedManager.addMap('MapInfos.json');
     });
+
 
     function getSystemData(dataPass) {
         if (!$gameTemp.isPlaytest()) {
@@ -151,83 +325,7 @@ Imported.NUUN_SystemDatabaseUnlimited = true;
             "data":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
             "events":[
             ]
-            };
-    };
-
-    NuunManager.addMap = function(dataPass, CreateMapJson) {
-        try {
-            const dataFile = getSystemData(dataPass);
-            const length = dataFile.length;
-            while (true) {
-                if (!dataFile[length]) {
-                    break;
-                }
-                length++;
-            }
-            this.outputMapJson(length);
-            dataFile.push(getMapSystemData(length));
-            const fs = require("fs");
-            const pass = "data/" + dataPass;
-            fs.writeFileSync(pass, JSON.stringify(dataFile));
-            const log = $gameSystem.isJapanese() ? "出力が完了しました。エディタを保存して確定してください。" : "Output completed. Save and confirm the editor.";
-            console.log(log);
-        } catch (e) {
-            const elog = $gameSystem.isJapanese() ? "エラーが発生しました。" : "An error has occurred";
-            console.log(elog);
-        }
-    };
-
-    NuunManager.addMapSystemData = function(dataPass, MaxDatabase, CreateMapJson) {
-        try {
-            const dataFile = getSystemData(dataPass);
-            const length = dataFile.length;
-            if (MaxDatabase > 0) {
-                for (let i = 1; i <= MaxDatabase; i++) {
-                    if (!dataFile[i]) {
-                        dataFile.push(getMapSystemData(i));
-                        this.outputMapJson(i);
-                    }
-                }
-            }
-            const fs = require("fs");
-            const pass = "data/" + dataPass;
-            fs.writeFileSync(pass, JSON.stringify(dataFile));
-            const log = $gameSystem.isJapanese() ? "出力が完了しました。エディタを保存して確定してください。" : "Output completed. Save and confirm the editor.";
-            console.log(log);
-        } catch (e) {
-            const elog = $gameSystem.isJapanese() ? "エラーが発生しました。" : "An error has occurred";
-            console.log(elog);
-        }
-    };
-
-    NuunManager.addSystemData = function(dataPass, MaxDatabase) {
-        try {
-            const dataFile = getSystemData(dataPass);
-            const length = dataFile.length;
-            if (MaxDatabase > 0) {
-                for (let i = 0; i <= MaxDatabase; i++) {
-                    if (!dataFile[i]) {
-                        dataFile.push(getAnimationsSystemData(i));
-                    }
-                }
-            }
-            const fs = require("fs");
-            const pass = "data/" + dataPass;
-            fs.writeFileSync(pass, JSON.stringify(dataFile));
-            const log = $gameSystem.isJapanese() ? "出力が完了しました。エディタを保存して確定してください。" : "Output completed. Save and confirm the editor.";
-            console.log(log);
-        } catch (e) {
-            const elog = $gameSystem.isJapanese() ? "エラーが発生しました。" : "An error has occurred";
-            console.log(elog);
-        }
-    };
-
-    NuunManager.outputMapJson = function(index) {
-        const filename = "Map%1".format(index.padZero(3));
-        const dataFile = getMapData();
-        const fs = require("fs");
-        const pass = "data/" + filename + ".json";
-        fs.writeFileSync(pass, JSON.stringify(dataFile));
+        };
     };
     
 })();
